7d0fba57391790f14c70447f890c2f88c7d10ce8
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2022-02-01 15:58:37 +0200

    GUI
    wxPython GUI tutorial

---
+++ Python Tutorial Programmes/GUI/wxPythonTut.py
@@ -0,0 +1,53 @@
+import wx
+
+# app = wx.App()
+#
+# frame = wx.Frame(parent=None, title='Hello World')
+# frame.Show()
+#
+# app.MainLoop()
+
+# class MyFrame(wx.Frame):
+#     def __init__(self):
+#         super().__init__(parent=None, title='Hello World')
+#         self.Show()
+#
+# if __name__ == '__main__':
+#     app = wx.App()
+#     frame = MyFrame()
+#     app.MainLoop()\
+
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='Hello World')
+
+        panel = wx.Panel(self) # Wx panel
+
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.text_ctrl = wx.TextCtrl(panel) # Text field
+        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+
+        my_btn = wx.Button(panel, label='Press Me') # Button
+        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+
+        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
+        panel.SetSizer(my_sizer)
+
+        self.Show()
+
+    def on_press(self, event):
+        value = self.text_ctrl.GetValue()
+
+        if not value:
+            print("You didn't enter anything!")
+        else:
+            print(f'You typed: "{value}"')
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
+
+
+
+
