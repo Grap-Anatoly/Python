d37b62ff9b28ddb3b1ee37273a7cd3ea046cf3af
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-06-12 15:10:15 +0300

    LeetCode tasks, day 45

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -3710,3 +3710,27 @@ def decompressRLElist(nums):
             res.append(val[i])
 
     return res
+
+# Given an array of integers arr, replace each element with its rank.
+#
+# The rank represents how large the element is. The rank has the following rules:
+# Rank is an integer starting from 1.
+# The larger the element, the larger the rank. If two elements are equal, their rank must be the same.
+# Rank should be as small as possible.
+def arrayRankTransform(arr):
+
+    arrSet = sorted(arr)
+    arrDct = {}
+
+    pointr = 1
+
+    for i in arrSet:
+        if i not in arrDct:
+            arrDct[i] = pointr
+            pointr += 1
+
+    res = []
+    for i in arr:
+        res.append(arrDct[i])
+
+    return res
