1bed5df6e3aedd7c7a0332465f963d2ed9de3600
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-06-15 16:52:54 +0300

    LeetCode tasks, day 48

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -3777,3 +3777,21 @@ def reformatDate(date):
     res += day
 
     return res
+
+# A bus has n stops numbered from 0 to n - 1 that form a circle. We know the distance between all pairs of
+# neighboring stops where distance[i] is the distance between the stops number i and (i + 1) % n.
+# The bus goes along both directions i.e. clockwise and counterclockwise.
+# Return the shortest distance between the given start and destination stops.
+def distanceBetweenBusStops(distance, start, destination):
+
+    if start > destination:
+        start, destination = destination, start
+
+    if start == len(distance) - 1:
+        left = sum(distance[start:] + distance[:destination])
+    else:
+        left = sum(distance[start:destination])
+
+    right = sum(distance[:start] + distance[destination:])
+
+    return min(left, right)
