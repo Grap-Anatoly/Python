3ac47746b1705217eaf1a99690e89ea78a6d2944
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-06-17 16:55:03 +0300

    LeetCode tasks, day 50

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -3837,3 +3837,21 @@ def threeConsecutiveOdds(arr):
             return True
 
     return False
+
+# Given a string text, you want to use the characters of text to form as many instances of the word "balloon"
+# as possible.
+# You can use each character in text at most once. Return the maximum number of instances that can be formed.
+def maxNumberOfBalloons(text):
+
+    balloon = {"b": 0, "a": 0, "l": 0, "o": 0, "n": 0}
+
+    for i in text:
+        if i in balloon:
+            balloon[i] += 1
+
+    balloon["l"] = balloon["l"] // 2
+    balloon["o"] = balloon["o"] // 2
+
+    mn = min(balloon, key=balloon.get)
+
+    return balloon[mn]
