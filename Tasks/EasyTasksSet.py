3b2d596e873116fcbc78d4b2645e3869f662fa1f
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-06-22 17:02:25 +0300

    LeetCode tasks, day 52

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -3871,3 +3871,23 @@ def makeFancyString(s):
                 res += i
 
     return res
+
+# Given an array of distinct integers arr, find all pairs of elements with the minimum
+# absolute difference of any two elements.
+def minimumAbsDifference(arr):
+
+    arr = sorted(arr)
+    minDiff = arr[1] - arr[0]
+
+    for i in range(1, len(arr)):
+        diff = arr[i] - arr[i - 1]
+        if diff < minDiff:
+            minDiff = diff
+
+    res = []
+    for i in range(1, len(arr)):
+        diff = arr[i] - arr[i - 1]
+        if diff == minDiff:
+            res.append([arr[i - 1], arr[i]])
+
+    return sorted(res)
