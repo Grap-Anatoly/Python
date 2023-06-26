842369fc39fbdd3d80773db662872998b275d9b7
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-06-26 17:01:26 +0300

    LeetCode tasks, day 55

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -3926,3 +3926,22 @@ def balancedStringSplit(s):
             res += 1
 
     return res
+
+# You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point.
+# Check if these points make a straight line in the XY plane.
+def checkStraightLine(coordinates):
+
+    l = len(coordinates)
+    if l == 2:
+        return True
+
+    xStep = coordinates[1][0] - coordinates[0][0]
+    yStep = coordinates[1][1] - coordinates[0][1]
+
+    for i in range(l):
+        x = coordinates[i][0] - coordinates[i - 1][0]
+        y = coordinates[i][1] - coordinates[i - 1][1]
+        if y * xStep != x * yStep:
+            return False
+
+    return True
