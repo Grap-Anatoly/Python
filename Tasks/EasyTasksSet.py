3d26a925f1a35554accd2bf5c495549cbfa679fe
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-07-07 16:06:44 +0300

    LeetCode tasks, day 66

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -4289,3 +4289,21 @@ def smallerNumbersThanCurrent(nums):
         res.append(temp)
 
     return res
+
+# Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
+#
+# The distance value is defined as the number of elements arr1[i]
+# such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
+def findTheDistanceValue(arr1, arr2, d):
+
+    res = 0
+    for i in arr1:
+        counter = 0
+        for j in arr2:
+            if abs(j - i) <= d:
+                counter += 1
+
+        if counter == 0:
+            res += 1
+
+    return res
