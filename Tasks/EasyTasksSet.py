c387e17b8a664aaa482fbfe0901a3f3fe5a68c5a
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-07-17 16:40:15 +0300

    LeetCode tasks, day 75

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -4534,3 +4534,26 @@ def maxPower(s):
                     temp = 1
         return max(res)
 
+# You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi
+# to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+#
+# It is guaranteed that the graph of paths forms a line without any loop, therefore,
+# there will be exactly one destination city.
+def destCity(paths):
+
+    cities = {"Start": [], "End": []}
+
+    for i in paths:
+        for j in range(len(i)):
+            if j == 0 and i[j] not in cities:
+                temp = cities["Start"]
+                temp.append(i[j])
+                cities["Start"] = temp
+            else:
+                temp = cities["End"]
+                temp.append(i[j])
+                cities["End"] = temp
+
+    diff = set(cities["End"]).difference(set(cities["Start"]))
+
+    return "".join(diff)
