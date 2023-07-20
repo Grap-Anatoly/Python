fde6db4c1fd744a12bfca6abd72b09059e1ecfae
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-07-20 16:18:52 +0300

    LeetCode tasks, day 78

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -4585,3 +4585,23 @@ def canBeEqual(target, arr):
             return False
 
     return True
+
+# Given two integer arrays startTime and endTime and given an integer queryTime.
+#
+# The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+#
+# Return the number of students doing their homework at time queryTime. More formally,
+# return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+def busyStudent(startTime, endTime, queryTime):
+
+    res = 0
+    for i in range(len(startTime)):
+
+        timeRange = []
+        for j in range(startTime[i], endTime[i] + 1):
+            timeRange.append(j)
+
+        if queryTime in timeRange:
+            res += 1
+
+    return res
