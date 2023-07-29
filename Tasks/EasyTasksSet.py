e24cfad94473c07fbb1d5fac92245b67ec4fa429
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-07-29 16:37:19 +0300

    LeetCode tasks, day 86

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -4716,3 +4716,23 @@ def xorOperation(n, start):
         res = res ^ i
 
     return res
+
+# A sequence of numbers is called an arithmetic progression if the difference between any
+# two consecutive elements is the same.
+#
+# Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression.
+# Otherwise, return false.
+def canMakeArithmeticProgression(arr):
+
+    arr = sorted(arr)[::-1]
+    diff = arr[0] - arr[1]
+
+    for i in range(len(arr)):
+        if i < len(arr) - 1:
+            if arr[i] - arr[i + 1] != diff:
+                return False
+        else:
+            if arr[i - 1] - arr[i] != diff:
+                return False
+
+    return True
