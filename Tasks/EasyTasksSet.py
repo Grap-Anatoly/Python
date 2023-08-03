f120a902354cbba5add94a4f01f4cfc6cefca040
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-08-03 16:13:35 +0300

    LeetCode tasks, day 91

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -4789,3 +4789,26 @@ def restoreString(s, indices):
 
     return "".join(res)
 
+# Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+#
+# A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+#
+# 0 <= i < j < k < arr.length
+# |arr[i] - arr[j]| <= a
+# |arr[j] - arr[k]| <= b
+# |arr[i] - arr[k]| <= c
+# Where |x| denotes the absolute value of x.
+#
+# Return the number of good triplets.
+def countGoodTriplets(arr, a, b, c):
+
+    res = 0
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            for k in range(len(arr)):
+                if 0 <= i < j < k < len(arr):
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        res += 1
+
+    return res
+
