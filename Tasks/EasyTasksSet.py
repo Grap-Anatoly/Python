414c681a5cfc63e8414b2988f9ae97d72bdb5971
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-08-13 15:40:37 +0300

    LeetCode tasks, day 99

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -4978,3 +4978,39 @@ class ParkingSystem:
             else:
                 return False
 
+# You are given a string text of words that are placed among some number of spaces.
+# Each word consists of one or more lowercase English letters and are separated by at least one space.
+# It's guaranteed that text contains at least one word.
+#
+# Rearrange the spaces so that there is an equal number of spaces between every pair of
+# adjacent words and that number is maximized. If you cannot redistribute all the spaces equally,
+# place the extra spaces at the end, meaning the returned string should be the same length as text.
+#
+# Return the string after rearranging the spaces.
+def reorderSpaces(text):
+
+    textList = list(text)
+
+    sps = 0
+    for i in textList:
+        if i == " ":
+            sps += 1
+
+    text = text.split()
+    words = len(text)
+
+    if words <= 1:
+        return "".join(text) + " " * sps
+    else:
+        spLen = sps // (len(text) - 1)
+
+        res = ""
+        for i in range(len(text)):
+            if i < len(text) - 1:
+                res += text[i] + " " * spLen
+                sps -= spLen
+            else:
+                res += text[i] + " " * sps
+
+        return res
+
