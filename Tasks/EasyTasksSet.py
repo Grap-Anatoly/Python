83770e77696b7e7fba74b0419ddd2954e7562f7f
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-08-18 17:18:08 +0300

    LeetCode tasks, day 103

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -5082,3 +5082,39 @@ def maxLengthBetweenEqualCharacters(s):
             if s[i] == s[j]:
                 res = max(res, j - i - 1)
     return res
+
+# A newly designed keypad was tested, where a tester pressed a sequence of n keys, one at a time.
+#
+# You are given a string keysPressed of length n, where keysPressed[i] was the ith key pressed in the testing sequence,
+# and a sorted list releaseTimes, where releaseTimes[i] was the time the ith key was released.
+# Both arrays are 0-indexed. The 0th key was pressed at the time 0, and every subsequent
+# key was pressed at the exact time the previous key was released.
+#
+# The tester wants to know the key of the keypress that had the longest duration.
+# The ith keypress had a duration of releaseTimes[i] - releaseTimes[i - 1],
+# and the 0th keypress had a duration of releaseTimes[0].
+#
+# Note that the same key could have been pressed multiple times during the test,
+# and these multiple presses of the same key may not have had the same duration.
+#
+# Return the key of the keypress that had the longest duration.
+# If there are multiple such keypresses, return the lexicographically largest key of the keypresses.
+def slowestKey(releaseTimes, keysPressed):
+
+    res = {}
+    for i in range(len(releaseTimes)):
+        if i == 0:
+            res[keysPressed[i]] = releaseTimes[i]
+        else:
+            if keysPressed[i] in res:
+                res[keysPressed[i]] = max(res[keysPressed[i]], releaseTimes[i] - releaseTimes[i - 1])
+            else:
+                res[keysPressed[i]] = releaseTimes[i] - releaseTimes[i - 1]
+
+    resList = []
+    mx = max(res, key=res.get)
+    for k, v in res.items():
+        if v == res[mx]:
+            resList.append(k)
+
+    return max(resList)
