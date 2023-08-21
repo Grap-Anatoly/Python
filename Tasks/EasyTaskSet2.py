e7870f1c6465c616861dbda7390b758fefc00062
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-08-21 17:44:49 +0300

    LeetCode tasks, day 106

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -46,3 +46,20 @@ def canFormArray(arr, pieces):
         else:
             return False
     return True
+
+# For a string sequence, a string word is k-repeating if word concatenated k times is a substring of sequence.
+# The word's maximum k-repeating value is the highest value k where word is k-repeating in sequence.
+# If word is not a substring of sequence, word's maximum k-repeating value is 0.
+#
+# Given strings sequence and word, return the maximum k-repeating value of word in sequence.
+def maxRepeating(self, sequence: str, word: str) -> int:
+
+    res = 0
+    sub = word
+
+    while len(sub) <= len(sequence):
+        if sub in sequence:
+            res += 1
+        sub += word
+
+    return res
