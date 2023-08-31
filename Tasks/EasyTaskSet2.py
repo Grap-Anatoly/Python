09f91b3c0b50b6c6fb4b6a1d9b2f31d5097adc84
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-08-31 16:46:16 +0300

    LeetCode tasks, day 114

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -202,3 +202,33 @@ def reformatNumber(number):
             formatted = formatted[3:]
 
     return res
+
+# Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+#
+# He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday,
+# he will put in $1 more than the day before. On every subsequent Monday,
+# he will put in $1 more than the previous Monday.
+# Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+def totalMoney(n):
+
+    res = 0
+    money = 1
+    counter = 0
+    week = 1
+
+    for i in range(n):
+        if counter < 7:
+            counter += 1
+
+            res += money
+            money += 1
+        else:
+            counter = 1
+            week += 1
+
+            money = week
+            res += money
+            money += 1
+
+    return res
+
