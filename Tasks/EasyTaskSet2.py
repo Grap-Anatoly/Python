2931180f845f5b9918fc8053d0647f5d0e3f83de
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-01 16:11:00 +0300

    LeetCode tasks, day 115

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -232,3 +232,29 @@ def totalMoney(n):
 
     return res
 
+# You are given a string s of even length. Split this string into two halves of equal lengths,
+# and let a be the first half and b be the second half.
+#
+# Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U').
+# Notice that s contains uppercase and lowercase letters.
+#
+# Return true if a and b are alike. Otherwise, return false.
+def halvesAreAlike(s):
+
+    vowels = "aeiouAEIOU"
+
+    sa = list(s)[0: len(s) // 2]
+    sb = list(s)[len(s) // 2:]
+
+    va = 0
+    vb = 0
+
+    for i in range(len(sa)):
+        if sa[i] in vowels:
+            va += 1
+        if sb[i] in vowels:
+            vb += 1
+
+    return va == vb
+
+
