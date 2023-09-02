74942584e1383366dd97db8b1fb1746799a9e592
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-02 16:23:03 +0300

    LeetCode tasks, day 116

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -257,4 +257,32 @@ def halvesAreAlike(s):
 
     return va == vb
 
+# You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes,
+# where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+#
+# numberOfBoxesi is the number of boxes of type i.
+# numberOfUnitsPerBoxi is the number of units in each box of the type i.
+# You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck.
+# You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+#
+# Return the maximum total number of units that can be put on the truck.
+def maximumUnits(boxTypes, truckSize):
+
+    boxTypes = sorted(boxTypes, key=lambda x: x[1])[::-1]
+
+    counter = 0
+    res = 0
+
+    for i in boxTypes:
+        boxes = i[0]
+        while boxes > 0:
+            if counter < truckSize:
+                res += i[1]
+                counter += 1
+                boxes -= 1
+            else:
+                return res
+
+    return res
+
 
