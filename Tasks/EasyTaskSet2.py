15d7c70e07850346c250eac18a4fb01f4785c890
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-03 16:09:52 +0300

    LeetCode tasks, day 117

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -285,4 +285,20 @@ def maximumUnits(boxTypes, truckSize):
 
     return res
 
+# There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+# The biker starts his trip on point 0 with altitude equal 0.
+#
+# You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i
+# and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+def largestAltitude(gain):
+
+    res = [0]
+
+    for i in range(len(gain)):
+        res.append(res[-1] + gain[i])
+
+    return max(res)
+
+
+
 
