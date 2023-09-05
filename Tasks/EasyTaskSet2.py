6775d7f8b65089b3a68f0898a6d6174cbf3b32ca
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-05 16:54:35 +0300

    LeetCode tasks, day 119

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -315,4 +315,21 @@ def decodeXOR(encoded, first):
 
     return res
 
+# You are given an array rectangles where rectangles[i] = [li, wi] represents
+# the ith rectangle of length li and width wi.
+#
+# You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example,
+# if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
+#
+# Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+#
+# Return the number of rectangles that can make a square with a side length of maxLen.
+def countGoodRectangles(rectangles):
+
+    res = []
+    for i in rectangles:
+        res.append(min(i))
+
+    return res.count(max(res))
+
 
