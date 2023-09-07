b2d662258adb928b756c7da99c57279849953f96
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-07 17:24:28 +0300

    LeetCode tasks, day 120

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -332,4 +332,29 @@ def countGoodRectangles(rectangles):
 
     return res.count(max(res))
 
+# You are working in a ball factory where you have n balls numbered from lowLimit up to highLimit
+# inclusive (i.e., n == highLimit - lowLimit + 1), and an infinite number of boxes numbered from 1 to infinity.
+#
+# Your job at this factory is to put each ball in the box with a number equal to the sum of digits of the ball's number.
+# For example, the ball number 321 will be put in the box number 3 + 2 + 1 = 6 and the ball number
+# 10 will be put in the box number 1 + 0 = 1.
+#
+# Given two integers lowLimit and highLimit, return the number of balls in the box with the most balls.
+def countBalls(lowLimit, highLimit):
+
+    res = {}
+    for i in range(lowLimit, highLimit + 1):
+
+        temp = 0
+        for j in str(i):
+            temp += int(j)
+
+        if temp in res:
+            res[temp] += 1
+        else:
+            res[temp] = 1
+
+    return res[max(res, key=res.get)]
+
+
 
