f311442c0745bcd1b53b8529f2410655f92ddb50
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-11 17:43:13 +0300

    LeetCode tasks, day 123

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -406,5 +406,25 @@ def maximumTime(time):
 
     return "".join(hours) + ":" + "".join(minutes)
 
+# Given an array nums, return true if the array was originally sorted in non-decreasing order,
+# then rotated some number of positions (including zero). Otherwise, return false.
+#
+# There may be duplicates in the original array.
+#
+# Note: An array A rotated by x positions results in an array B of the same length such that
+# A[i] == B[(i+x) % A.length], where % is the modulo operation.
+def check(nums):
+
+    srt = sorted(nums)
+
+    for i in range(len(nums)):
+        temp = srt[-1]
+        srt.pop()
+        srt.insert(0, temp)
+        if srt == nums:
+            return True
+
+    return False
+
 
 
