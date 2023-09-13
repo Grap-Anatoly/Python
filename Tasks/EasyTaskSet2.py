c75f85f66eb66e87a6f4b95fb297724666081194
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-13 17:19:24 +0300

    LeetCode tasks, day 125

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -452,5 +452,25 @@ def minOperations(s):
 
     return min(forvard, reverse)
 
+# You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y).
+# You are also given an array points where each points[i] = [ai, bi] represents that a point exists at (ai, bi).
+# A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
+#
+# Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location.
+# If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
+#
+# The Manhattan distance between two points (x1, y1) and (x2, y2) is abs(x1 - x2) + abs(y1 - y2).
+def nearestValidPoint(x, y, points):
+
+    res = {}
+    for i in range(len(points)):
+        if x == points[i][0] or y == points[i][1]:
+            res[i] = abs(x - points[i][0]) + abs(y - points[i][1])
+
+    if len(res) != 0:
+        return min(res, key=res.get)
+    else:
+        return -1
+
 
 
