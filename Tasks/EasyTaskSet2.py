819cd0e902c58fe0e9cef9672fa028894ff3aa4a
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-21 16:54:50 +0300

    LeetCode tasks, day 133

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -639,4 +639,36 @@ def maxAscendingSum(nums):
 
         return max(res)
 
+# You are given a string word that consists of digits and lowercase English letters.
+#
+# You will replace every non-digit character with a space. For example, "a123bc34d8ef34" will become " 123  34 8  34".
+# Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+#
+# Return the number of different integers after performing the replacement operations on word.
+#
+# Two integers are considered different if their decimal representations without any leading zeros are different.
+def numDifferentIntegers(word):
+
+    numbers = "1234567890"
+
+    t = ""
+    integers = []
+
+    for i in range(len(word)):
+        if word[i] in numbers:
+            t += word[i]
+        else:
+            integers.append(t)
+            t = ""
+
+    if len(t) > 0:
+        integers.append(t)
+
+    res = []
+    for i in integers:
+        if i != '' and int(i) not in res:
+            res.append(int(i))
+
+    return len(res)
+
 
