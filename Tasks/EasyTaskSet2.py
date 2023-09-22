d056a011e52de2bada46e2d7dd1af27b544b330b
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-22 16:29:56 +0300

    LeetCode tasks, day 134

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -671,4 +671,42 @@ def numDifferentIntegers(word):
 
     return len(res)
 
+# You are given an integer array nums (0-indexed). In one operation, you can choose an element of the
+# array and increment it by 1.
+#
+# For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+# Return the minimum number of operations needed to make nums strictly increasing.
+#
+# An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1.
+# An array of length 1 is trivially strictly increasing.
+def minOperations(nums):
+
+    res = 0
+    for i in range(len(nums)):
+        if i < len(nums) - 1:
+            if nums[i] >= nums[i + 1]:
+                while nums[i] >= nums[i + 1]:
+                    nums[i + 1] = nums[i + 1] + 1
+                    res += 1
+        else:
+            if nums[i] < nums[i - 1]:
+                while nums[i] < nums[i - 1]:
+                    nums[i] = nums[i] + 1
+                    res += 1
+
+    return res
+
+# Faster ver
+def minOperationsFast(nums):
+
+    res = 0
+    for i in range(1, len(nums)):
+        if nums[i] <= nums[i - 1]:
+            c = nums[i]
+            nums[i] += (nums[i - 1] - nums[i]) + 1
+            res += nums[i] - c
+
+    return res
+
+
 
