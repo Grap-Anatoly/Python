408efec2c5a77f122ff1466905561a7543b6cab6
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-09-29 18:11:12 +0300

    LeetCode tasks, day 139

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -791,6 +791,20 @@ def checkIfPangram(sentence):
     else:
         return False
 
+# Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n
+# from base 10 to base k.
+#
+# After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
+def sumBase(n, k):
+
+    res = 0
+    while n:
+        res += n % k
+        n //= k
+
+    return res
+
+
 
 
 
