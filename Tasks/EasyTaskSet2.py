4398d02ec628431e121afb2e9bb967e7879cba71
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-05 18:32:23 +0300

    LeetCode tasks, day 145

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -941,4 +941,24 @@ def checkZeroOnes(s):
     else:
         return False
 
+# You are given a 2D integer array ranges and two integers left and right. Each ranges[i] = [starti, endi]
+# represents an inclusive interval between starti and endi.
+#
+# Return true if each integer in the inclusive range [left, right] is covered by at least one interval in ranges.
+# Return false otherwise.
+#
+# An integer x is covered by an interval ranges[i] = [starti, endi] if starti <= x <= endi.
+def isCovered(ranges, left, right):
+
+    for i in range(left, right + 1):
+        present = False
+        for j in ranges:
+            if j[0] <= i and i <= j[1]:
+                present = True
+
+        if present == False:
+            return False
+
+    return True
+
 
