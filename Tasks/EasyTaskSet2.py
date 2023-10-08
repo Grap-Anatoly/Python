9ba34a4ce328f5dcdab06d3b6ff1ed83b225ebf7
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-08 15:22:51 +0300

    LeetCode tasks, day 148

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1021,3 +1021,17 @@ def findRotation(mat, target):
                 return True
 
     return False
+
+# Given a 0-indexed integer array nums, return true if it can be made strictly increasing after removing exactly
+# one element, or false otherwise. If the array is already strictly increasing, return true.
+#
+# The array nums is strictly increasing if nums[i - 1] < nums[i] for each index (1 <= i < nums.length).
+def canBeIncreasing(nums):
+
+    for i in range(len(nums)):
+        temp = nums.copy()
+        temp.pop(i)
+        if temp == sorted(set(temp)):
+            return True
+
+    return False
