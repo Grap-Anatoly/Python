40159b5a68d119c28c58f1b50ca0c0848d5a3fe5
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-09 17:10:42 +0300

    LeetCode tasks, day 149

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1035,3 +1035,27 @@ def canBeIncreasing(nums):
             return True
 
     return False
+
+# You are given an array of strings words (0-indexed).
+#
+# In one operation, pick two distinct indices i and j, where words[i] is a non-empty string,
+# and move any character from words[i] to any position in words[j].
+#
+# Return true if you can make every string in words equal using any number of operations, and false otherwise.
+def makeEqual(words):
+
+    l = len(words)
+    s = {}
+
+    for i in words:
+        for j in i:
+            if j in s:
+                s[j] += 1
+            else:
+                s[j] = 1
+
+    for v in s.values():
+        if v % l != 0:
+            return False
+
+    return True
