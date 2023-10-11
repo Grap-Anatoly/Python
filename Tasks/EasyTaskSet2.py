fe7f24e9aaf4f9b87d2b22587451f76c24cbd975
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-11 16:50:37 +0300

    LeetCode tasks, day 151

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1076,3 +1076,15 @@ def countTriples(n):
                     res += 1
 
     return res
+
+# The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+#
+# For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+# Given an integer array nums, choose four distinct indices w, x, y, and z such that the product
+# difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+#
+# Return the maximum such product difference.
+def maxProductDifference(nums):
+    nums = sorted(nums)
+
+    return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
