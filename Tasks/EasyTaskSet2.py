ba6755b8f2ef7f030b7316f4a342451edf808d38
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-12 17:58:46 +0300

    LeetCode tasks, day 152

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1088,3 +1088,14 @@ def maxProductDifference(nums):
     nums = sorted(nums)
 
     return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
+
+# Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] =
+# nums[nums[i]] for each 0 <= i < nums.length and return it.
+#
+# A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+def buildArray(nums):
+    res = []
+    for i in range(len(nums)):
+        res.append(nums[nums[i]])
+
+    return res
