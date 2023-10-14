028465bd32a6d4ba04c29a82cf84fcbf1c42c978
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-14 15:45:49 +0300

    LeetCode tasks, day 153

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1119,3 +1119,23 @@ def areOccurrencesEqual(s):
         cList.append(v)
 
     return min(cList) == max(cList)
+
+# Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and
+# ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+#
+# Specifically, ans is the concatenation of two nums arrays.
+#
+# Return the array ans.
+def getConcatenationViaLoop(nums):
+
+    ans = []
+
+    for i in range(2):
+        for j in nums:
+            ans.append(j)
+
+    return ans
+
+def getConcatenationEasy(nums):
+
+    return nums * 2
