217a22d17d84c519c99e77e338a862ea9c123e5a
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-15 15:51:19 +0300

    LeetCode tasks, day 154

--- Tasks/CheatSheet.py
+++ Tasks/CheatSheet.py
@@ -67,3 +67,17 @@ def hasPrefix(sentence, prefix):
             return sentence.index(s) + 1
 
     return "There is no prefix in input sentence"
+
+
+"""
+ Dictionary of english alphabet with its positional numbers 
+ as a keys
+
+    Key = "a" > Number = 1
+    Key = "c" > Number = 3
+"""
+
+
+letters = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11,
+           "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21,
+           "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1139,3 +1139,39 @@ def getConcatenationViaLoop(nums):
 def getConcatenationEasy(nums):
 
     return nums * 2
+
+# You are given a string s consisting of lowercase English letters, and an integer k.
+#
+# First, convert s into an integer by replacing each letter with its position in the alphabet (i.e.,
+# replace 'a' with 1, 'b' with 2, ..., 'z' with 26). Then, transform the integer by replacing it with the sum of
+# its digits. Repeat the transform operation k times in total.
+#
+# For example, if s = "zbax" and k = 2, then the resulting integer would be 8 by the following operations:
+#
+# Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124
+# Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17
+# Transform #2: 17 ➝ 1 + 7 ➝ 8
+# Return the resulting integer after performing the operations described above.
+def getLucky(s, k):
+
+    words = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11,
+             "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21,
+             "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
+
+    numbers = ""
+
+    def convert(number):
+        n = 0
+        for i in list(number):
+            n += int(i)
+
+        return str(n)
+
+    for i in list(s):
+        numbers += str(words[i])
+
+    for i in range(k):
+        numbers = convert(numbers)
+
+    return int(numbers)
+
