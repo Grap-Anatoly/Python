a41dc11c93082c0fc62e3c2e0c691b73e08315d3
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-17 16:09:49 +0300

    LeetCode tasks, day 156

--- Tasks/EasyTasksSet.py
+++ Tasks/EasyTasksSet.py
@@ -5137,3 +5137,19 @@ def isThree(n):
             break
 
     return res == 3
+
+# Given a string s and an array of strings words, determine whether s is a prefix string of words.
+#
+# A string s is a prefix string of words if s can be made by concatenating the first k strings in words for 
+# some positive k no larger than words.length.
+#
+# Return true if s is a prefix string of words, or false otherwise.
+def isPrefixString(s, words) :
+
+    res = ""
+    for i in words:
+        res += i
+        if res == s:
+            return True
+
+    return False
