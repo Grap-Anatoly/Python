5030edad95103875769159b74ec07face36879b1
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-10-23 16:09:28 +0300

    LeetCode tasks, day 160

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1209,3 +1209,22 @@ def countQuadruplets(nums):
 
     return res
 
+
+# Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the
+# index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+#
+# For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3
+# (inclusive). The resulting string will be "dcbaefd".
+# Return the resulting string.
+def reversePrefix(sword, ch):
+
+    word = list(word)
+    if ch in word:
+        pref = "".join(word[:word.index(ch) + 1][::-1])
+        left = "".join(word[word.index(ch) + 1:])
+        return pref + left
+
+    else:
+        return "".join(word)
+
+
