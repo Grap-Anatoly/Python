23db8673bda6f6ad7b0eb8067d7bbc3a6b3901e6
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-11-06 18:41:01 +0300

    LeetCode tasks, day 173

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1507,3 +1507,22 @@ def timeRequiredToBuy(tickets, k):
             counter = 0
 
     return res
+
+# There are n houses evenly lined up on the street, and each house is beautifully painted.
+# You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith
+# house.
+#
+# Return the maximum distance between two houses with different colors.
+#
+# The distance between the ith and jth houses is abs(i - j), where abs(x) is the absolute value of x.
+def maxDistance(colors):
+
+    res = []
+
+    for i in colors:
+        for j in colors[::-1]:
+            if i != j:
+                e = colors[::-1].index(j)
+                res.append((len(colors) - e - 1) - colors.index(i))
+
+    return max(res)
