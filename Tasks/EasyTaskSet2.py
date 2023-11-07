fb3da2e0d532b240f4fbbffca7b068f58076a162
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-11-07 17:28:11 +0300

    LeetCode tasks, day 174

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1526,3 +1526,18 @@ def maxDistance(colors):
                 res.append((len(colors) - e - 1) - colors.index(i))
 
     return max(res)
+
+
+# You are given an integer array nums and an integer k. You want to find a subsequence of nums of length k
+# that has the largest sum.
+#
+# Return any such subsequence as an integer array of length k.
+#
+# A subsequence is an array that can be derived from another array by deleting some or no elements without
+# changing the order of the remaining elements.
+def maxSubsequence(nums, k):
+
+    while len(nums) != k:
+        nums.remove(min(nums))
+
+    return nums
