c780fe8d528fcfc3c834384e251d3e4226d55137
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-11-10 20:47:33 +0300

    LeetCode tasks, day 177

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1575,3 +1575,37 @@ def mostWordsFound(sentences):
         res.append(len(i.split()))
 
     return max(res)
+
+# There are n rings and each ring is either red, green, or blue. The rings are distributed across ten rods
+# labeled from 0 to 9.
+#
+# You are given a string rings of length 2n that describes the n rings that are placed onto the rods. Every two
+# characters in rings forms a color-position pair that is used to describe each ring where:
+#
+# The first character of the ith pair denotes the ith ring's color ('R', 'G', 'B').
+# The second character of the ith pair denotes the rod that the ith ring is placed on ('0' to '9').
+# For example, "R3G2B1" describes n == 3 rings: a red ring placed onto the rod labeled 3, a green ring placed onto
+# the rod labeled 2, and a blue ring placed onto the rod labeled 1.
+#
+# Return the number of rods that have all three colors of rings on them.
+def countPoints(rings):
+
+    col = "BGR"
+    ringsDict = {}
+
+    for i in range(len(rings)):
+        if rings[i] in col:
+            if rings[i + 1] in ringsDict:
+                t = ringsDict[rings[i + 1]]
+                if rings[i] not in t:
+                    t += rings[i]
+                    ringsDict[rings[i + 1]] = t
+            else:
+                ringsDict[rings[i + 1]] = rings[i]
+
+    res = 0
+    for v in ringsDict.values():
+        if sorted(v) == sorted(col):
+            res += 1
+
+    return res
