1040830aa3cc35b4ae8c084458410dd887b1ac14
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-11-11 18:08:36 +0300

    LeetCode tasks, day 178

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1609,3 +1609,16 @@ def countPoints(rings):
             res += 1
 
     return res
+
+
+# Given an array of strings words, return the first palindromic string in the array.
+# If there is no such string, return an empty string "".
+#
+# A string is palindromic if it reads the same forward and backward.
+def firstPalindrome(words):
+
+    for i in words:
+        if i == i[::-1]:
+            return i
+
+    return ""
