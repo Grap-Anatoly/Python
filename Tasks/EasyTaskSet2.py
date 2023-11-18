2e0c8714128ad5b81ac2d705e11ddb535a978f9b
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-11-18 18:11:40 +0300

    LeetCode tasks, day 185

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1751,3 +1751,18 @@ def divideString(s, k, fill):
 
     return res
 
+# You are given a positive integer num consisting of exactly four digits. Split num into two new
+# integers new1 and new2 by using the digits found in num. Leading zeros are allowed in new1 and new2,
+# and all the digits found in num must be used.
+#
+# For example, given num = 2932, you have the following digits: two 2's, one 9 and one 3. Some of the possible pairs
+# [new1, new2] are [22, 93], [23, 92], [223, 9] and [2, 329].
+# Return the minimum possible sum of new1 and new2.
+def minimumSum(num):
+
+    num = sorted(list(str(num)))
+    pair1 = int(num[0] + num[3])
+    pair2 = int(num[1] + num[2])
+
+    return pair1 + pair2
+
