88a3cd89ef7d41f90637b1ab975332a7aef10a2c
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-11-25 17:41:08 +0300

    LeetCode tasks, day 191

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1867,5 +1867,20 @@ def countOperations(num1, num2):
                 num2 -= num1
                 res += 1
 
+# You are given an array of strings words and a string pref.
+#
+# Return the number of strings in words that contain pref as a prefix.
+#
+# A prefix of a string s is any leading contiguous substring of s.
+def prefixCount(words, pref):
+
+    res = 0
+    for i in words:
+        if i[0:len(pref)] == pref:
+            res += 1
+
+    return res
+
+
 
 
