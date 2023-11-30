8ce90b84a5e8057b9e30611e0922a0734e0993ca
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-11-30 18:57:18 +0300

    LeetCode tasks, day 195

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -1942,7 +1942,24 @@ def divideArray(nums):
 
     return True
 
+# You are given a 0-indexed integer array nums. You are also given an integer key, which is present in nums.
+#
+# For every unique integer target in nums, count the number of times target immediately follows an occurrence of
+# key in nums. In other words, count the number of indices i such that:
+#
+# 0 <= i <= nums.length - 2,
+# nums[i] == key and,
+# nums[i + 1] == target.
+# Return the target with the maximum count. The test cases will be generated such that the target with maximum
+# count is unique.
+def mostFrequent(nums, key):
 
+    c = Counter()
+    for i, n in enumerate(nums):
+        if n == key and i + 1 < len(nums):
+            c[nums[i + 1]] += 1
+
+    return c.most_common(1)[0][0]
 
 
 
