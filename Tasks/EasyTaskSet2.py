1f719e5216c686f518fad5f7339af114d9c93277
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-12-08 17:11:19 +0300

    LeetCode tasks, day 202

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2093,4 +2093,17 @@ def removeDigit(number, digit):
 
     return str(mx)
 
+# You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+#
+# Return the number of strings in words that are a prefix of s.
+#
+# A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence
+# of characters within a string.
+def countPrefixes(words, s):
 
+    res = 0
+    for i in words:
+        if i in s[0:len(i)]:
+            res += 1
+
+    return res
