0baf5666ddc385f81f3fb7ee1f42dfc3f3df2185
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-12-11 19:50:32 +0300

    LeetCode tasks, day 205

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2152,3 +2152,45 @@ def findClosestNumber(nums):
         return sorted(bigger)[0]
     else:
         return sorted(lower)[-1]
+
+# You are given a string s consisting of digits and an integer k.
+#
+# A round can be completed if the length of s is greater than k. In one round, do the following:
+#
+# Divide s into consecutive groups of size k such that the first k characters are in the first group,
+# the next k characters are in the second group, and so on. Note that the size of the last group can be smaller than k.
+# Replace each group of s with a string representing the sum of all its digits. For example, "346" is
+# replaced with "13" because 3 + 4 + 6 = 13.
+# Merge consecutive groups together to form a new string. If the length of the string is greater than k,
+# repeat from step 1.
+# Return s after all rounds have been completed.
+def digitSum(s, k):
+
+    s = list(s)
+
+    def splitIntoKChunks(s, k):
+        res = []
+        temp = []
+
+        for i in range(len(s)):
+            if len(temp) < k:
+                temp.append(s[i])
+            else:
+                res.append("".join(temp))
+                temp = [s[i]]
+
+        if len(temp) != 0:
+            res.append("".join(temp))
+
+        return res
+
+    while len(s) > k:
+        r = splitIntoKChunks(s, k)
+        s = ""
+        for i in r:
+            t = 0
+            for v in i:
+                t += int(v)
+            s += str(t)
+
+    return "".join(s)
