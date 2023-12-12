6df9730b45a15aa2a56df921aa327b90e16f33dd
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-12-12 17:06:11 +0300

    LeetCode tasks, day 206

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2194,3 +2194,11 @@ def digitSum(s, k):
             s += str(t)
 
     return "".join(s)
+
+# Given a string s and a character letter, return the percentage of characters in s that equal letter
+# rounded down to the nearest whole percent.
+def percentageLetter(s, letter):
+
+    c = s.count(letter)
+
+    return int(c / len(s) * 100)
