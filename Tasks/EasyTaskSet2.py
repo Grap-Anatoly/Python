8722581135e9ad7edf145d37160318265c6ccaa5
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-12-20 19:24:29 +0300

    LeetCode tasks, day 211

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2251,3 +2251,27 @@ def sum(self, num1, num2):
 # Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
 def checkTree(root):
     return root.val == root.left.val + root.right.val
+
+# You are given a 0-indexed integer array nums whose length is a power of 2.
+#
+# Apply the following algorithm on nums:
+#
+# Let n be the length of nums. If n == 1, end the process. Otherwise, create a new 0-indexed integer array newNums of
+# length n / 2.
+# For every even index i where 0 <= i < n / 2, assign the value of newNums[i] as min(nums[2 * i], nums[2 * i + 1]).
+# For every odd index i where 0 <= i < n / 2, assign the value of newNums[i] as max(nums[2 * i], nums[2 * i + 1]).
+# Replace the array nums with newNums.
+# Repeat the entire process starting from step 1.
+# Return the last number that remains in nums after applying the algorithm.
+def minMaxGame(nums):
+
+    while len(nums) != 1:
+        newNums = []
+        for i in range(len(nums) // 2):
+            if i % 2 == 0:
+                newNums.append(min(nums[2 * i], nums[2 * i + 1]))
+            else:
+                newNums.append(max(nums[2 * i], nums[2 * i + 1]))
+        nums = newNums
+
+    return nums[0]
