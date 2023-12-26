7f41ffca0e5d03c0bc2e1506008e93e1307ef254
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-12-26 18:37:43 +0300

    LeetCode tasks, day 216

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2395,3 +2395,23 @@ def decodeMessage(key, message):
                 res.append(' ')
 
         return "".join(res)
+
+# You have a water dispenser that can dispense cold, warm, and hot water. Every second, you can either fill up
+# 2 cups with different types of water, or 1 cup of any type of water.
+#
+# You are given a 0-indexed integer array amount of length 3 where amount[0], amount[1], and amount[2] denote the
+# number of cold, warm, and hot water cups you need to fill respectively. Return the minimum number of seconds needed
+# to fill up all the cups.
+def fillCups(amount):
+    res = 0
+    while amount != [0, 0, 0]:
+        amount = sorted(amount)[::-1]
+        if amount[1] > 0:
+            amount[0] -= 1
+            amount[1] -= 1
+            res += 1
+        else:
+            amount[0] -= 1
+            res += 1
+
+    return res
