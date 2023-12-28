5512fe5f73ebd23025ae9d609e3f73f362bedd19
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-12-28 20:33:20 +0300

    LeetCode tasks, day 218

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2449,3 +2449,29 @@ def evaluateTree(root):
         return evaluateTree(root.left) or evaluateTree(root.right)
     else:
         return evaluateTree(root.left) and evaluateTree(root.right)
+
+# You are given a 0-indexed integer array nums. In one operation, you may do the following:
+#
+# Choose two integers in nums that are equal.
+# Remove both integers from nums, forming a pair.
+# The operation is done on nums as many times as possible.
+#
+# Return a 0-indexed integer array answer of size 2 where answer[0] is the number of pairs that are formed and
+# answer[1] is the number of leftover integers in nums after doing the operation as many times as possible.
+def numberOfPairs(nums):
+
+    res = [0, 0]
+
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j:
+                if nums[i] == nums[j] and nums[i] != "":
+                    res[0] = res[0] + 1
+                    nums[i] = ""
+                    nums[j] = ""
+
+    for i in nums:
+        if i != "":
+            res[1] += 1
+
+    return res
