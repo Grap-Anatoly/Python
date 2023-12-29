6f45807cc505e752ae052c9a62fdd4af4d6b1064
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-12-29 21:33:28 +0300

    LeetCode tasks, day 219

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2475,3 +2475,18 @@ def numberOfPairs(nums):
             res[1] += 1
 
     return res
+
+# Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+#
+# Note:
+#
+# A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
+# s will contain at least one letter that appears twice.
+def repeatedCharacter(s):
+
+    temp = []
+    for i in s:
+        if i in temp:
+            return i
+        else:
+            temp.append(i)
