db1fc93c9737851cb6de0617929abb9f0659ac51
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2023-12-31 19:27:12 +0300

    LeetCode tasks, day 220

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2490,3 +2490,33 @@ def repeatedCharacter(s):
             return i
         else:
             temp.append(i)
+
+# You are given an integer array ranks and a character array suits. You have 5 cards where the ith card has a
+# rank of ranks[i] and a suit of suits[i].
+#
+# The following are the types of poker hands you can make from best to worst:
+#
+# "Flush": Five cards of the same suit.
+# "Three of a Kind": Three cards of the same rank.
+# "Pair": Two cards of the same rank.
+# "High Card": Any single card.
+# Return a string representing the best type of poker hand you can make with the given cards.
+#
+# Note that the return values are case-sensitive.
+def bestHand(self, ranks: List[int], suits: List[str]) -> str:
+
+    res = {"Flush": 0, "Three of a Kind": 0, "Pair": 0, "High Card": 0}
+
+    if len(set(suits)) == 1:
+        res['Flush'] = 1
+    for i in set(ranks):
+        if ranks.count(i) >= 3:
+            res['Three of a Kind'] = 1
+        if ranks.count(i) == 2:
+            res['Pair'] = 1
+    else:
+        res['High Card'] = 1
+
+    for k, v in res.items():
+        if v == 1:
+            return k
