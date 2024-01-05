593c0f89f7681d671ff9a016cb8c7099b581d8cd
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-05 17:28:34 +0300

    LeetCode tasks, day 224

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2585,3 +2585,22 @@ def mergeSimilarItems(items1, items2):
         resList.append([k, v])
 
     return sorted(resList)
+
+# You are given an n x n integer matrix grid.
+#
+# Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
+#
+# maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+# In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+#
+# Return the generated matrix.
+def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+
+    res = [[1] * (len(grid) - 2) for i in range(len(grid) - 2)]
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid) - 1):
+            res[i - 1][j - 1] = max(grid[i - 1][j - 1], grid[i - 1][j], grid[i - 1][j + 1], grid[i][j - 1],
+                                    grid[i][j], grid[i][j + 1], grid[i + 1][j - 1], grid[i + 1][j],
+                                    grid[i + 1][j + 1])
+
+    return res
