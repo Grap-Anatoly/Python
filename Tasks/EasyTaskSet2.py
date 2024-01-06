c4be4859f9cd351b1dd5bee1400d06254e2764b3
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-06 16:57:40 +0300

    LeetCode tasks, day 225

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2604,3 +2604,43 @@ def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
                                     grid[i + 1][j + 1])
 
     return res
+
+# You are entering a competition, and are given two positive integers initialEnergy and initialExperience denoting
+# your initial energy and initial experience respectively.
+#
+# You are also given two 0-indexed integer arrays energy and experience, both of length n.
+#
+# You will face n opponents in order. The energy and experience of the ith opponent is denoted by energy[i] and
+# experience[i] respectively. When you face an opponent, you need to have both strictly greater experience and energy
+# to defeat them and move to the next opponent if available.
+#
+# Defeating the ith opponent increases your experience by experience[i], but decreases your energy by energy[i].
+#
+# Before starting the competition, you can train for some number of hours. After each hour of training, you can either
+# choose to increase your initial experience by one, or increase your initial energy by one.
+#
+# Return the minimum number of training hours required to defeat all n opponents.
+def minNumberOfHours(initialEnergy, initialExperience, energy, experience):
+
+    res = 0
+    opponents = 0
+
+    while opponents < len(energy):
+        if initialExperience > experience[opponents] and initialEnergy > energy[opponents]:
+            initialEnergy -= energy[opponents]
+            initialExperience += experience[opponents]
+            opponents += 1
+        else:
+            while initialEnergy <= energy[opponents]:
+                initialEnergy += 1
+                res += 1
+            while initialExperience <= experience[opponents]:
+                initialExperience += 1
+                res += 1
+
+            if initialExperience > experience[opponents] and initialEnergy > energy[opponents]:
+                initialEnergy -= energy[opponents]
+                initialExperience += experience[opponents]
+                opponents += 1
+
+    return res
