df903830b3b30ddd2f186ee0b0e6f3214c66f83a
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-07 18:27:47 +0300

    LeetCode tasks, day 226

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2644,3 +2644,24 @@ def minNumberOfHours(initialEnergy, initialExperience, energy, experience):
                 opponents += 1
 
     return res
+
+# You are given a 0-indexed string blocks of length n, where blocks[i] is either 'W' or 'B', representing the color of
+# the ith block. The characters 'W' and 'B' denote the colors white and black, respectively.
+#
+# You are also given an integer k, which is the desired number of consecutive black blocks.
+#
+# In one operation, you can recolor a white block such that it becomes a black block.
+#
+# Return the minimum number of operations needed such that there is at least one occurrence of k consecutive black
+# blocks.
+def minimumRecolors(blocks, k):
+    if "B" * k in blocks:
+        return 0
+    else:
+        blocks = list(blocks)
+        res = []
+
+        for i in range(len(blocks) - k + 1):
+            res.append(blocks[i:i + k].count("W"))
+
+        return min(res)
