b1f45e3a7de09be546f1ce02fb0c37490c6034c5
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-13 15:04:12 +0300

    LeetCode tasks, day 230

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2759,4 +2759,13 @@ def mostFrequentEven(nums):
     else:
         return -1
 
+# Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+def smallestEvenMultiple(n):
+
+    if n % 2 == 0:
+        return n
+    else:
+        return n * 2
+
+
 
