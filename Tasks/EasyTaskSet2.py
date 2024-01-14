355431e0fb33b2d34d9e54763db94262988246a4
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-14 15:22:21 +0300

    LeetCode tasks, day 231

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2767,5 +2767,29 @@ def smallestEvenMultiple(n):
     else:
         return n * 2
 
+# Alice and Bob are traveling to Rome for separate business meetings.
+#
+# You are given 4 strings arriveAlice, leaveAlice, arriveBob, and leaveBob. Alice will be in the city from the dates
+# arriveAlice to leaveAlice (inclusive), while Bob will be in the city from the dates arriveBob to leaveBob
+# (inclusive). Each will be a 5-character string in the format "MM-DD", corresponding to the month and day of the date.
+#
+# Return the total number of days that Alice and Bob are in Rome together.
+#
+# You can assume that all dates occur in the same calendar year, which is not a leap year. Note that the number of days
+# per month can be represented as: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].
+def countDaysTogether(self, arriveAlice: str, leaveAlice: str, arriveBob: str, leaveBob: str) -> int:
+
+    res = 0
+    res = max(0, self.getDate(min(leaveAlice, leaveBob)) - self.getDate(max(arriveAlice, arriveBob)) + 1)
+
+    return res
+
+def getDate(self, date):
+    monthList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month = int(date[:2])
+    days = int(date[3:])
+    return sum(monthList[: month - 1]) + days
+
+
 
 
