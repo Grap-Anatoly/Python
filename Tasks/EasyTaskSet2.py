e3398cbc4f096bd710e97b2ca5a0000465229922
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-15 16:31:11 +0300

    LeetCode tasks, day 232

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2790,6 +2790,25 @@ def getDate(self, date):
     days = int(date[3:])
     return sum(monthList[: month - 1]) + days
 
+# You are given an array of strings names, and an array heights that consists of distinct positive integers.
+# Both arrays are of length n.
+#
+# For each index i, names[i] and heights[i] denote the name and height of the ith person.
+#
+# Return names sorted in descending order by the people's heights.
+def sortPeople(names, heights):
+
+    heightsSet = []
+    for i in range(len(names)):
+        heightsSet.append([heights[i], names[i]])
+
+    res = []
+    for i in sorted(heightsSet)[::-1]:
+        res.append(i[1])
+
+    return res
+
+
 
 
 
