46c7fda15f6b981afee8414368ae890bfb8ba908
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-25 18:17:49 +0300

    LeetCode tasks, day 241

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -2985,6 +2985,22 @@ def applyOperations(nums):
 
     return res
 
+# You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the
+# temperature in Celsius.
+#
+# You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+#
+# Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+#
+# Note that:
+#
+# Kelvin = Celsius + 273.15
+# Fahrenheit = Celsius * 1.80 + 32.00
+def convertTemperature(celsius):
+
+    return [celsius + 273.15, celsius * 1.80 + 32.00]
+
+
 
 
 
