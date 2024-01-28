c6031e990545368ef6ee4821efd0f93ce34f9a67
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-28 15:13:19 +0300

    LeetCode tasks, day 244

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3040,6 +3040,22 @@ def unequalTriplets(nums):
 
     return res
 
+# Given a positive integer n, find the pivot integer x such that:
+#
+# The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+# Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most
+# one pivot index for the given input.
+#
+#
+def pivotInteger(n):
+
+    for i in range(1, n + 1):
+
+        if sum(range(1, i + 1)) == sum(range(i, n + 1)):
+            return i
+
+    return -1
+
 
 
 
