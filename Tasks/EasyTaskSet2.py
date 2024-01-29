9afb2eac17455b59d4f7a1edd4b24b0d1e0f830c
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-29 15:44:18 +0300

    LeetCode tasks, day 245

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3056,6 +3056,21 @@ def pivotInteger(n):
 
     return -1
 
+# A valid cut in a circle can be:
+#
+# A cut that is represented by a straight line that touches two points on the edge of the circle and passes through
+# its center, or
+# A cut that is represented by a straight line that touches one point on the edge of the circle and its center.
+# Some valid and invalid cuts are shown in the figures below.
+def numberOfCuts(n):
+    if n == 1:
+        return 0
+    else:
+        if n % 2 == 0:
+            return n // 2
+        else:
+            return n
+
 
 
 
