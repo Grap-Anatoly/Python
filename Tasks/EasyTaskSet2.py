3b66b110506fe281bd3f52a9f949c0944cf92130
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-30 15:24:35 +0300

    LeetCode tasks, day 246

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3071,6 +3071,35 @@ def numberOfCuts(n):
         else:
             return n
 
+# A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+#
+# For example, "Hello World", "HELLO", "hello world hello world" are all sentences.
+# Words consist of only uppercase and lowercase English letters. Uppercase and lowercase English letters are
+# considered different.
+#
+# A sentence is circular if:
+#
+# The last character of a word is equal to the first character of the next word.
+# The last character of the last word is equal to the first character of the first word.
+# For example, "leetcode exercises sound delightful", "eetcode", "leetcode eats soul" are all circular sentences.
+# However, "Leetcode is cool", "happy Leetcode", "Leetcode" and "I like Leetcode" are not circular sentences.
+#
+# Given a string sentence, return true if it is circular. Otherwise, return false.
+def isCircularSentence(sentence):
+    sentence = sentence.split()
+    if len(sentence) == 1:
+        return sentence[0][0] == sentence[0][-1]
+    else:
+        for i in range(len(sentence)):
+            if i != len(sentence) - 1:
+                if sentence[i][-1] != sentence[i + 1][0]:
+                    return False
+            else:
+                if sentence[i][-1] != sentence[0][0]:
+                    return False
+
+        return True
+
 
 
 
