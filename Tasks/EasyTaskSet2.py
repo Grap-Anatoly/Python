e85ede809f1d56c653a450bfdc459e99a4019051
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-01-31 17:37:58 +0300

    LeetCode tasks, day 247

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3100,6 +3100,30 @@ def isCircularSentence(sentence):
 
         return True
 
+# You are given an m x n matrix grid consisting of positive integers.
+#
+# Perform the following operation until grid becomes empty:
+#
+# Delete the element with the greatest value from each row. If multiple such elements exist, delete any of them.
+# Add the maximum of deleted elements to the answer.
+# Note that the number of columns decreases by one after each operation.
+#
+# Return the answer after performing the operations described above.
+def deleteGreatestValue(grid):
+
+    for i in range(len(grid)):
+        grid[i] = sorted(grid[i])[::-1]
+
+    res = []
+    for i in range(len(grid[0])):
+        temp = []
+        for j in range(len(grid)):
+            temp.append(grid[j][0])
+            grid[j].pop(0)
+        res.append(max(temp))
+
+    return sum(res)
+
 
 
 
