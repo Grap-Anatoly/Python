5019753c2d4f27fa6ff71b61f2c5de65699a3477
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-03 16:18:53 +0300

    LeetCode tasks, day 249

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3146,6 +3146,24 @@ def maximumValue(strs):
 
     return max(res)
 
+# You are given a 0-indexed string array words.
+#
+# Two strings are similar if they consist of the same characters.
+#
+# For example, "abca" and "cba" are similar since both consist of characters 'a', 'b', and 'c'.
+# However, "abacba" and "bcfd" are not similar since they do not consist of the same characters.
+# Return the number of pairs (i, j) such that 0 <= i < j <= word.length - 1 and the two strings words[i]
+# and words[j] are similar.
+def similarPairs(words):
+
+    res = 0
+    for i in range(len(words)):
+        for j in range(len(words)):
+            if i != j and set(words[i]) == set(words[j]):
+                res += 1
+
+    return res // 2
+
 
 
 
