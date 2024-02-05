32a07dfaff49cec3880d5a5bce1c67f732c6396e
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-05 16:11:00 +0300

    LeetCode tasks, day 251

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3197,6 +3197,53 @@ def closetTarget(words, target, startIndex):
     else:
         return -1
 
+# You are given a 0-indexed integer array forts of length n representing the positions of several forts.
+# forts[i] can be -1, 0, or 1 where:
+#
+# -1 represents there is no fort at the ith position.
+# 0 indicates there is an enemy fort at the ith position.
+# 1 indicates the fort at the ith the position is under your command.
+# Now you have decided to move your army from one of your forts at position i to an empty position j such that:
+#
+# 0 <= i, j <= n - 1
+# The army travels over enemy forts only. Formally, for all k where min(i,j) < k < max(i,j), forts[k] == 0.
+# While moving the army, all the enemy forts that come in the way are captured.
+#
+# Return the maximum number of enemy forts that can be captured. In case it is impossible to move your army,
+# or you do not have any fort under your command, return 0.
+def captureForts(forts):
+
+    res = []
+    i = 0
+    while i < len(forts):
+        if forts[i] == 1 and i != len(forts) - 1:
+            pointer = 0
+            i += 1
+            while forts[i] == 0 and i != len(forts) - 1:
+                pointer += 1
+                i += 1
+            if forts[i] == -1:
+                res.append(pointer)
+        else:
+            i += 1
+
+    i = len(forts) - 1
+    while i > 0:
+        if forts[i] == 1 and i != 0:
+            pointer = 0
+            i -= 1
+            while forts[i] == 0:
+                pointer += 1
+                i -= 1
+            if i >= 0 and forts[i] == -1:
+                res.append(pointer)
+        else:
+            i -= 1
+
+    if len(res) > 0:
+        return max(res)
+    else:
+        return 0
 
 
 
