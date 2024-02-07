cc59e081fe9a70b612b9b66d85c3ebf2bda7d620
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-07 16:37:33 +0300

    LeetCode tasks, day 253

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3257,6 +3257,24 @@ def countDigits(num):
 
     return res
 
+# Given an array nums sorted in non-decreasing order, return the maximum between the number of positive
+# integers and the number of negative integers.
+#
+# In other words, if the number of positive integers in nums is pos and the number of negative integers is neg,
+# then return the maximum of pos and neg.
+# Note that 0 is neither positive nor negative.
+def maximumCount(nums):
+
+    res = [0, 0]
+    for i in nums:
+        if i < 0:
+            res[0] += 1
+        elif i > 0:
+            res[1] += 1
+
+    return max(res)
+
+
 
 
 
