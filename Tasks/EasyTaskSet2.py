26fa52247280e8038232361b2ad96e9d88122a4e
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-08 17:49:52 +0300

    LeetCode tasks, day 254

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3274,6 +3274,35 @@ def maximumCount(nums):
 
     return max(res)
 
+# Given four integers length, width, height, and mass, representing the dimensions and mass of a box,
+# respectively, return a string representing the category of the box.
+#
+# The box is "Bulky" if:
+# Any of the dimensions of the box is greater or equal to 104.
+# Or, the volume of the box is greater or equal to 109.
+# If the mass of the box is greater or equal to 100, it is "Heavy".
+# If the box is both "Bulky" and "Heavy", then its category is "Both".
+# If the box is neither "Bulky" nor "Heavy", then its category is "Neither".
+# If the box is "Bulky" but not "Heavy", then its category is "Bulky".
+# If the box is "Heavy" but not "Bulky", then its category is "Heavy".
+# Note that the volume of the box is the product of its length, width and height.
+def categorizeBox(length, width, height, mass):
+
+    res = []
+    if length >= 10 ** 4 or width >= 10 ** 4 or height >= 10 ** 4 or length * width * height >= 10 ** 9:
+        res.append("Bulky")
+    if mass >= 100:
+        res.append("Heavy")
+
+    if "Bulky" in res and "Heavy" in res:
+        return "Both"
+
+    if "Bulky" not in res and "Heavy" not in res:
+        return "Neither"
+
+    return res[0]
+
+
 
 
 
