b024a06ea4674a1ae4de8730338d1c1679eabf9c
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-09 17:27:04 +0300

    LeetCode tasks, day 255

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3302,6 +3302,24 @@ def categorizeBox(length, width, height, mass):
 
     return res[0]
 
+# You are given a positive integer array nums.
+#
+# The element sum is the sum of all the elements in nums.
+# The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+# Return the absolute difference between the element sum and digit sum of nums.
+#
+# Note that the absolute difference between two integers x and y is defined as |x - y|.
+def differenceOfSum(nums):
+
+    res = [0, 0]
+    for i in nums:
+        res[0] += i
+        for j in str(i):
+            res[1] += int(j)
+
+    return abs(res[0] - res[1])
+
+
 
 
 
