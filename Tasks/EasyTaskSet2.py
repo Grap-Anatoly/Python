6a6f03b4b0ebe53598d628c0273b4dd434f0c2e7
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-11 18:04:39 +0300

    LeetCode tasks, day 257

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3335,6 +3335,21 @@ def alternateDigitSum(n):
 
     return res
 
+# Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to
+# both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+#
+# Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that
+# integer.
+def getCommon(nums1, nums2):
+
+    сommon = []
+    common = list(set(nums1).intersection(set(nums2)))
+
+    if len(common) > 0:
+        return min(common)
+    else:
+        return -1
+
 
 
 
