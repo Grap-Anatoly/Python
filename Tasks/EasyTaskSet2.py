40ef47bba90db765e5b6db6fe8c356102dfb8177
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-13 18:08:10 +0300

    LeetCode tasks, day 258

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3350,6 +3350,23 @@ def getCommon(nums1, nums2):
     else:
         return -1
 
+# Given an array of positive integers nums, return an array answer that consists of the digits of each integer
+# in nums after separating them in the same order they appear in nums.
+#
+# To separate the digits of an integer is to get all the digits it has in the same order.
+#
+# For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+def separateDigits(nums):
+
+    res = []
+    for i in nums:
+        if len(str(i)) > 1:
+            for j in str(i):
+                res.append(int(j))
+        else:
+            res.append(i)
+
+    return res
 
 
 
