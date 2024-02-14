3d752a1575b8d5a3051bc322c9f7fa0be62334cb
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-14 20:00:47 +0300

    LeetCode tasks, day 259

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3368,6 +3368,46 @@ def separateDigits(nums):
 
     return res
 
+# There are n people standing in a line labeled from 1 to n. The first person in the line is holding a
+# pillow initially. Every second, the person holding the pillow passes it to the next person standing in
+# the line. Once the pillow reaches the end of the line, the direction changes, and people continue passing
+# the pillow in the opposite direction.
+#
+# For example, once the pillow reaches the nth person they pass it to the n - 1th person, then to the n -
+# 2th person and so on.
+# Given the two positive integers n and time, return the index of the person holding the pillow after time seconds.
+def passThePillow(n, time):
+
+    counter = 1
+    direction = "left"
+    for i in range(time):
+        action = 1
+
+        if direction == "left" and counter < n and action > 0:
+            counter += 1
+
+            action = 0
+
+        if direction == "left" and counter == n and action > 0:
+            direction = "right"
+            counter -= 1
+
+            action = 0
+
+        if direction == "right" and counter > 1 and action > 0:
+            counter -= 1
+
+            action = 0
+
+        if direction == "right" and counter == 1 and action > 0:
+            direction = "left"
+            counter += 1
+
+            action = 0
+
+    return counter
+
+
 
 
 
