bd789e8b72ab1e3b9e4fe2e1524d2f867d00960c
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-16 18:20:53 +0300

    LeetCode tasks, day 261

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3428,6 +3428,25 @@ def splitNum(num):
         return s
 
 
+# You are given a 0-indexed array of string words and two integers left and right.
+#
+# A string is called a vowel string if it starts with a vowel character and ends with a vowel character
+# where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+#
+# Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+def vowelStrings(words, left, right):
+    vowels = "aeiou"
+    res = 0
+    for i in range(len(words)):
+        if i in range(left, right + 1):
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                res += 1
+        elif i > right:
+            break
+
+    return res
+
+
 
 
 
