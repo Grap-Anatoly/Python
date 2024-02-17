004fc1f2f5e7e94f58a9392dcf524dae18d89c68
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-17 17:50:43 +0300

    LeetCode tasks, day 262

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3446,6 +3446,27 @@ def vowelStrings(words, left, right):
 
     return res
 
+# You are given a positive integer n.
+#
+# Let even denote the number of even indices in the binary representation of n (0-indexed) with value 1.
+#
+# Let odd denote the number of odd indices in the binary representation of n (0-indexed) with value 1.
+#
+# Return an integer array answer where answer = [even, odd].
+def evenOddBit(n):
+
+    binN = str(bin(n))[2:][::-1]
+    res = [0, 0]
+
+    for i in range(len(binN)):
+        if binN[i] == "1":
+            if i % 2 == 0:
+                res[0] += 1
+            else:
+                res[1] += 1
+
+    return res
+
 
 
 
