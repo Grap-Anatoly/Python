53eb2443869c1aca98beaab545689691e8307872
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-22 16:42:34 +0300

    LeetCode tasks, day 266

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3521,6 +3521,55 @@ def distinctIntegers(n):
     else:
         return 1
 
+# You are given two 0-indexed integer arrays player1 and player2, that represent the number of pins that
+# player 1 and player 2 hit in a bowling game, respectively.
+#
+# The bowling game consists of n turns, and the number of pins in each turn is exactly 10.
+#
+# Assume a player hit xi pins in the ith turn. The value of the ith turn for the player is:
+#
+# 2xi if the player hit 10 pins in any of the previous two turns.
+# Otherwise, It is xi.
+# The score of the player is the sum of the values of their n turns.
+#
+# Return
+#
+# 1 if the score of player 1 is more than the score of player 2,
+# 2 if the score of player 2 is more than the score of player 1, and
+# 0 in case of a draw.
+def isWinner(player1, player2):
+
+    if len(player1) == 1:
+        p1 = player1[0]
+        p2 = player2[0]
+
+        if p1 > p2:
+            return 1
+        elif p2 > p1:
+            return 2
+        else:
+            return 0
+
+    p1, p2 = 0, 0
+    for i in range(0, len(player1)):
+        if (i > 0 and player1[i - 1] == 10) or (i > 1 and player1[i - 2] == 10):
+            p1 += player1[i] * 2
+        else:
+            p1 += player1[i]
+
+        if (i > 0 and player2[i - 1] == 10) or (i > 1 and player2[i - 2] == 10):
+            p2 += player2[i] * 2
+        else:
+            p2 += player2[i]
+
+    if p1 > p2:
+        return 1
+    elif p2 > p1:
+        return 2
+    else:
+        return 0
+
+
 
 
 
