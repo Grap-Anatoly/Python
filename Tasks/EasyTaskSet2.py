8b7fab819d76cd919ba2efb94bb6c6b2ffc74b3b
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-26 18:23:27 +0300

    LeetCode tasks, day 270

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3641,6 +3641,37 @@ def findTheArrayConcVal(nums):
 
     return res
 
+# You are given an integer num. You know that Bob will sneakily remap one of the 10 possible digits (0 to 9) to
+# another digit.
+#
+# Return the difference between the maximum and minimum values Bob can make by remapping exactly one digit in num.
+#
+# Notes:
+#
+# When Bob remaps a digit d1 to another digit d2, Bob replaces all occurrences of d1 in num with d2.
+# Bob can remap a digit to itself, in which case num does not change.
+# Bob can remap different digits for obtaining minimum and maximum values respectively.
+# The resulting number after remapping can contain leading zeroes.
+def minMaxDifference(num):
+
+    mx = []
+    mn = []
+    num = str(num)
+
+    for i in set(num):
+        maxTemp = []
+        minTemp = []
+        for j in range(len(num)):
+            if num[j] == i:
+                minTemp.append("0")
+                maxTemp.append("9")
+            else:
+                maxTemp.append(num[j])
+                minTemp.append(num[j])
+        mx.append(int("".join(maxTemp)))
+        mn.append(int("".join(minTemp)))
+
+    return max(mx) - min(mn)
 
 
 
