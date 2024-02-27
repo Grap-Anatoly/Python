930356f552365885bb9a0ad7bc0a4cc530420cc8
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-27 23:16:14 +0300

    LeetCode tasks, day 271

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3673,6 +3673,34 @@ def minMaxDifference(num):
 
     return max(mx) - min(mn)
 
+# You are given two 2D integer arrays nums1 and nums2.
+#
+# nums1[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+# nums2[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+# Each array contains unique ids and is sorted in ascending order by id.
+#
+# Merge the two arrays into one array that is sorted in ascending order by id, respecting the following conditions:
+#
+# Only ids that appear in at least one of the two arrays should be included in the resulting array.
+# Each id should be included only once and its value should be the sum of the values of this id in the two arrays.
+# If the id does not exist in one of the two arrays then its value in that array is considered to be 0.
+# Return the resulting array. The returned array must be sorted in ascending order by id.
+#
+#
+def mergeArrays(nums1, nums2):
+
+    res = []
+    for i in nums1:
+        for j in nums2:
+            if i[0] == j[0]:
+                i[1] += j[1]
+                nums2.pop(nums2.index(j))
+
+    for i in nums2:
+        nums1.append(i)
+
+    return sorted(nums1)
+
 
 
 
