d0d395438c72924ca8891a593a249681776d0dbe
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-02-29 22:15:37 +0300

    LeetCode tasks, day 273

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3726,6 +3726,23 @@ def leftRightDifference(nums):
 
     return res
 
+# There is a bag that consists of items, each item has a number 1, 0, or -1 written on it.
+#
+# You are given four non-negative integers numOnes, numZeros, numNegOnes, and k.
+#
+# The bag initially contains:
+#
+# numOnes items with 1s written on them.
+# numZeroes items with 0s written on them.
+# numNegOnes items with -1s written on them.
+# We want to pick exactly k items among the available items. Return the maximum possible sum of
+# numbers written on the items.
+def kItemsWithMaximumSum(numOnes, numZeros, numNegOnes, k):
+
+    lst = [1] * numOnes + [0] * numZeros + [-1] * numNegOnes
+    return sum(lst[0:k])
+
+
 
 
 
