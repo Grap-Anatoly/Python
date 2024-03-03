91f0057c6663e5b77eb9801d4afec16a1add9e40
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-03 16:51:49 +0300

    LeetCode tasks, day 276

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3849,6 +3849,33 @@ def findTheLongestBalancedSubstring(s):
     else:
         return 0
 
+# You are given a 0-indexed array of strings details. Each element of details provides information about a given
+# passenger compressed into a string of length 15. The system is such that:
+#
+# The first ten characters consist of the phone number of passengers.
+# The next character denotes the gender of the person.
+# The following two characters are used to indicate the age of the person.
+# The last two characters determine the seat allotted to that person.
+# Return the number of passengers who are strictly more than 60 years old.
+def countSeniors(details):
+
+    res = 0
+    for i in details:
+        if "M" in i:
+            temp = i.split("M")
+            age = int(temp[1][:2])
+        elif "F" in i:
+            temp = i.split("F")
+            age = int(temp[1][:2])
+        else:
+            temp = i.split("O")
+            age = int(temp[1][:2])
+
+        if age > 60:
+            res += 1
+
+    return res
+
 
 
 
