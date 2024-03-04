df0d0b7035dcc685592f5ffda722b4e477c91f93
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-04 16:59:46 +0300

    LeetCode tasks, day 277

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3876,6 +3876,28 @@ def countSeniors(details):
 
     return res
 
+# Given a m x n binary matrix mat, find the 0-indexed position of the row that contains the maximum count
+# of ones, and the number of ones in that row.
+#
+# In case there are multiple rows that have the maximum count of ones, the row with the smallest row number
+# should be selected.
+#
+# Return an array containing the index of the row, and the number of ones in it.
+def rowAndMaximumOnes(mat):
+
+    numberOfOnes = []
+    for i in range(len(mat)):
+        numberOfOnes.append([mat[i].count(1), i])
+
+    m = sorted(numberOfOnes)[-1][0]
+    res = []
+
+    for i in numberOfOnes:
+        if i[0] == m:
+            res.append(i[1])
+
+    return [min(res), m]
+
 
 
 
