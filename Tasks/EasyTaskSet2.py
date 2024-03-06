eba18a2d821e2117e4f7937ab78d07a1468e7663
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-06 16:41:21 +0300

    LeetCode tasks, day 279

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3947,6 +3947,27 @@ def buyChoco(prices, money):
         return money - sum(prices[:2])
 
 
+# You are given a 0-indexed integer array nums and an integer k. Your task is to perform the following operation
+# exactly k times in order to maximize your score:
+#
+# Select an element m from nums.
+# Remove the selected element m from the array.
+# Add a new element with a value of m + 1 to the array.
+# Increase your score by m.
+# Return the maximum score you can achieve after performing the operation exactly k times.
+def maximizeSum(nums, k):
+
+        nums = sorted(nums)[::-1]
+        res = 0
+
+        for i in range(k):
+            res += nums[0]
+            nums[0] += 1
+
+        return res
+
+
+
 
 
 
