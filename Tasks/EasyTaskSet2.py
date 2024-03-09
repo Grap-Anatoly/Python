eece2c119c8a1f0f3fba3bb92d15119140e5eadc
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-09 16:43:09 +0300

    LeetCode tasks, day 281

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -3983,6 +3983,30 @@ def distinctDifferenceArray(nums):
 
     return res
 
+# You are given a 0-indexed permutation of n integers nums.
+#
+# A permutation is called semi-ordered if the first number equals 1 and the last number equals n. You can perform
+# the below operation as many times as you want until you make nums a semi-ordered permutation:
+#
+# Pick two adjacent elements in nums, then swap them.
+# Return the minimum number of operations to make nums a semi-ordered permutation.
+#
+# A permutation is a sequence of integers from 1 to n of length n containing each number exactly once.
+def semiOrderedPermutation(nums):
+
+    if nums == sorted(nums):
+        return 0
+    else:
+        minEl = nums.index(min(nums))
+        maxEl = nums.index(max(nums))
+
+        if minEl > maxEl:
+            return minEl - maxEl + len(nums) - 2
+        else:
+            return minEl - maxEl + len(nums) - 1
+
+
+
 
 
 
