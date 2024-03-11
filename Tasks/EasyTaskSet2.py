989069462786c15ca3cc7450fb10e50e370f6229
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-11 17:19:10 +0300

    LeetCode tasks, day 283

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4049,6 +4049,21 @@ def circularGameLosers(n, k):
 
         counter += 1
 
+# You are given a string s consisting only of uppercase English letters.
+#
+# You can apply some operations to this string where, in one operation, you can remove any occurrence of one of
+# the substrings "AB" or "CD" from s.
+#
+# Return the minimum possible length of the resulting string that you can obtain.
+#
+# Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+def minLength(s):
+
+    while "AB" in s or "CD" in s:
+        s = s.replace("AB", "")
+        s = s.replace("CD", "")
+
+    return len(s)
 
 
 
