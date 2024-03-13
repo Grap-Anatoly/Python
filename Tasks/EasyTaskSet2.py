ac022fe753e74f0c0f3587ad9b50fdaccb1d54c3
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-13 16:52:50 +0300

    LeetCode tasks, day 284

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4065,6 +4065,16 @@ def minLength(s):
 
     return len(s)
 
+# You are given two integers, num and t.
+#
+# An integer x is called achievable if it can become equal to num after applying the following operation no more than
+# t times:
+#
+# Increase or decrease x by 1, and simultaneously increase or decrease num by 1.
+# Return the maximum possible achievable number. It can be proven that there exists at least one achievable number.
+def theMaximumAchievableX(num, t):
+    return num + t * 2
+
 
 
 
