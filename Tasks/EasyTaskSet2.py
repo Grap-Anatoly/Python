2e1724b4b3c50d2c4a067c36839ee852d12e2694
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-14 17:54:07 +0300

    LeetCode tasks, day 285

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4075,6 +4075,33 @@ def minLength(s):
 def theMaximumAchievableX(num, t):
     return num + t * 2
 
+# You are given a string s consisting of lowercase English letters, and you are allowed to perform operations on it.
+# In one operation, you can replace a character in s with another lowercase English letter.
+#
+# Your task is to make s a palindrome with the minimum number of operations possible. If there are multiple
+# palindromes that can be made using the minimum number of operations, make the lexicographically smallest one.
+#
+# A string a is lexicographically smaller than a string b (of the same length) if in the first position where a and
+# b differ, string a has a letter that appears earlier in the alphabet than the corresponding letter in b.
+#
+# Return the resulting palindrome string.
+def makeSmallestPalindrome(s):
+
+    s = list(s)
+
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        if s[i] != s[j]:
+            if s[i] < s[j]:
+                s[j] = s[i]
+            else:
+                s[i] = s[j]
+        i += 1
+        j -= 1
+
+    return "".join(s)
 
 
 
