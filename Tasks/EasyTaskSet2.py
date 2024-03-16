01e259792cc902e540af567b51805c77edd940c4
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-16 16:37:08 +0300

    LeetCode tasks, day 287

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4112,6 +4112,25 @@ def removeTrailingZeros(num):
 
     return num
 
+# You are given an integer n that consists of exactly 3 digits.
+#
+# We call the number n fascinating if, after the following modification, the resulting number contains all the digits
+# from 1 to 9 exactly once and does not contain any 0's:
+#
+# Concatenate n with the numbers 2 * n and 3 * n.
+# Return true if n is fascinating, or false otherwise.
+#
+# Concatenating two numbers means joining them together. For example, the concatenation of 121 and 371 is 121371.
+def isFascinating(n):
+
+    res = ""
+    res += str(n) + str(2 * n) + str(3 * n)
+
+    if "".join(sorted(res)) == "123456789":
+        return True
+    else:
+        return False
+
 
 
 
