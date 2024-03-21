9817df5342ff72820f61cb863b0b0bfcb0de4387
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-21 16:38:26 +0300

    LeetCode tasks, day 292

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4210,6 +4210,35 @@ def maximumNumberOfStringPairs(words):
 
     return res
 
+# A truck has two fuel tanks. You are given two integers, mainTank representing the fuel present in the main tank in
+# liters and additionalTank representing the fuel present in the additional tank in liters.
+#
+# The truck has a mileage of 10 km per liter. Whenever 5 liters of fuel get used up in the main tank, if the additional
+# tank has at least 1 liters of fuel, 1 liters of fuel will be transferred from the additional tank to the main tank.
+#
+# Return the maximum distance which can be traveled.
+#
+# Note: Injection from the additional tank is not continuous. It happens suddenly and immediately for every 5 liters
+# consumed.
+def distanceTraveled(mainTank, additionalTank):
+
+    res = 0
+    count = 1
+
+    while mainTank > 0:
+
+        if count == 5 and additionalTank > 0:
+            mainTank += 1
+            additionalTank -= 1
+            count = 1
+        else:
+            count += 1
+
+        res += 10
+        mainTank -= 1
+
+    return res
+
 
 
 
