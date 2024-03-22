fe69f24ba0a10bb70effb0cbb60152c06e69f6a7
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-22 17:03:53 +0300

    LeetCode tasks, day 293

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4239,6 +4239,30 @@ def distanceTraveled(mainTank, additionalTank):
 
     return res
 
+# You are given a 0-indexed integer array nums and an integer threshold.
+#
+# Find the length of the longest subarray of nums starting at index l and ending at index r (0 <= l <= r < nums.length)
+# that satisfies the following conditions:
+#
+# nums[l] % 2 == 0
+# For all indices i in the range [l, r - 1], nums[i] % 2 != nums[i + 1] % 2
+# For all indices i in the range [l, r], nums[i] <= threshold
+# Return an integer denoting the length of the longest such subarray.
+#
+# Note: A subarray is a contiguous non-empty sequence of elements within an array.
+def longestAlternatingSubarray(nums, threshold):
+
+    res = 0
+    for l in range(len(nums)):
+        if nums[l] % 2 == 0 and nums[l] <= threshold:
+            r = l
+            while r + 1 < len(nums) and nums[r + 1] <= threshold and nums[r] % 2 != nums[r + 1] % 2:
+                r += 1
+            res = max(res, r - l + 1)
+
+    return res
+
+
 
 
 
