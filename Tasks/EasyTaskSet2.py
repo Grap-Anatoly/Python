9a5b444a1e8c453a0dc226b042079ef52feedc42
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-24 16:53:19 +0300

    LeetCode tasks, day 295

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4319,6 +4319,23 @@ def alternatingSubarray2(nums):
 
     return -1 if mx == 1 else mx
 
+# There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the
+# company.
+#
+# The company requires each employee to work for at least target hours.
+#
+# You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
+#
+# Return the integer denoting the number of employees who worked at least target hours.
+def numberOfEmployeesWhoMetTarget(hours, target):
+
+    res = 0
+    for i in hours:
+        if i >= target:
+            res += 1
+
+    return res
+
 
 
 
