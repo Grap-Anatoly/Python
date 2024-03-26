a2dd872b9da183cd03fbcbb9c2b4011d5cb42df9
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-26 17:36:37 +0300

    LeetCode tasks, day 297

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4374,7 +4374,24 @@ def finalString(s):
 
     return res
 
+# You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+#
+# base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which contains 1 to n - 1
+# exactly once, plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+#
+# Return true if the given array is good, otherwise return false.
+#
+# Note: A permutation of integers represents an arrangement of these numbers.
+def isGood(nums):
 
+    n = max(nums)
+    perm = [i for i in range(1, n + 1)]
+    perm.append(perm[-1])
+
+    if sorted(nums) == perm:
+        return True
+    else:
+        return False
 
 
 
