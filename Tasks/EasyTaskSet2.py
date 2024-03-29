058a4217d676745058409107e2b2aef39812d205
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-29 14:57:31 +0300

    LeetCode tasks, day 300

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4466,6 +4466,21 @@ def accountBalanceAfterPurchase(purchaseAmount):
 
     return 100 - purchaseAmount
 
+# Given an array of strings words and a string s, determine if s is an acronym of words.
+#
+# The string s is considered an acronym of words if it can be formed by concatenating the first character of each
+# string in words in order. For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from
+# ["bear", "aardvark"].
+#
+# Return true if s is an acronym of words, and false otherwise.
+def isAcronym(words, s):
+
+    res = []
+    for i in words:
+        res.append(i[0])
+
+    return "".join(res) == s
+
 
 
 
