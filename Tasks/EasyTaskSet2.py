6f66e2b5a91f8e5b21e0fddd0ec020a6133ade2a
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-30 16:44:17 +0300

    LeetCode tasks, day 301

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4481,6 +4481,29 @@ def isAcronym(words, s):
 
     return "".join(res) == s
 
+# You are given two positive integers low and high.
+#
+# An integer x consisting of 2 * n digits is symmetric if the sum of the first n digits of x is equal to the sum
+# of the last n digits of x. Numbers with an odd number of digits are never symmetric.
+#
+# Return the number of symmetric integers in the range [low, high].
+def countSymmetricIntegers(low, high):
+
+    res = 0
+    for i in range(low, high + 1):
+        num = str(i)
+        if len(num) % 2 == 0:
+            left = 0
+            right = 0
+            for i in range(len(num) // 2):
+                left += int(num[i])
+                right += int(num[-(i + 1)])
+
+            if left == right:
+                res += 1
+
+    return res
+
 
 
 
