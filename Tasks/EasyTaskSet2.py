8ce07c45698dba436321d8e9b717f75de010da3b
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-03-31 15:25:36 +0300

    LeetCode tasks, day 302

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4505,6 +4505,19 @@ def countSymmetricIntegers(low, high):
     return res
 
 
+# You are given two strings s1 and s2, both of length 4, consisting of lowercase English letters.
+#
+# You can apply the following operation on any of the two strings any number of times:
+#
+# Choose any two indices i and j such that j - i = 2, then swap the two characters at those indices in the string.
+# Return true if you can make the strings s1 and s2 equal, and false otherwise.
+def canBeEqual(s1, s2):
+
+    if sorted(s1[::2]) == sorted(s2[::2]) and sorted(s1[1::2]) == sorted(s2[1::2]):
+        return True
+    else:
+        return False
+
 
 
 
