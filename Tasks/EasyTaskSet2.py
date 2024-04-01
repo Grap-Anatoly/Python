020a62a227b56fa1329029bfd99a7e906a42d5e2
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-04-01 16:11:19 +0300

    LeetCode tasks, day 303

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4518,8 +4518,24 @@ def canBeEqual(s1, s2):
     else:
         return False
 
+# You are given a string moves of length n consisting only of characters 'L', 'R', and '_'.
+# The string represents your movement on a number line starting from the origin 0.
+#
+# In the ith move, you can choose one of the following directions:
+#
+# move to the left if moves[i] = 'L' or moves[i] = '_'
+# move to the right if moves[i] = 'R' or moves[i] = '_'
+# Return the distance from the origin of the furthest point you can get to after n moves.
+def furthestDistanceFromOrigin(moves):
 
+    lCount = moves.count("L")
+    rCount = moves.count("R")
+    uCount = moves.count("_")
 
+    if lCount >= rCount:
+        return abs(lCount - rCount) + uCount
+    else:
+        return abs(rCount - lCount) + uCount
 
 
 
