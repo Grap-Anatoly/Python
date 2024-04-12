10ebcd8715ab9e4348e8184addfe65d0f20a5d6e
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-04-12 15:41:39 +0300

    LeetCode tasks, day 312

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4708,6 +4708,37 @@ def sumCounts(nums):
 
     return res
 
+# Given an integer array nums where nums[i] is either a positive integer or -1. We need to find for each -1 the
+# respective positive integer, which we call the last visited integer.
+#
+# To achieve this goal, let's define two empty arrays: seen and ans.
+#
+# Start iterating from the beginning of the array nums.
+#
+# If a positive integer is encountered, prepend it to the front of seen.
+# If -1 is encountered, let k be the number of consecutive -1s seen so far (including the current -1),
+# If k is less than or equal to the length of seen, append the k-th element of seen to ans.
+# If k is strictly greater than the length of seen, append -1 to ans.
+# Return the array ans.
+def lastVisitedIntegers(nums):
+
+    seen = []
+    res = []
+    k = 0
+    for i in nums:
+        if i != -1:
+            seen.append(i)
+            k = 0
+        else:
+            k += 1
+            if k <= len(seen):
+                res.append(seen[-k])
+            else:
+                res.append(-1)
+
+    return res
+
+
 
 
 
