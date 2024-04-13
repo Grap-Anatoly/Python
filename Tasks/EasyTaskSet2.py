49f2987d3d97dbd7ed71e808f188f349bd1772e0
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-04-13 15:55:43 +0300

    LeetCode tasks, day 313

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4739,6 +4739,44 @@ def lastVisitedIntegers(nums):
     return res
 
 
+# You are given a 0-indexed integer array nums having length n, an integer indexDifference, and an integer
+# valueDifference.
+#
+# Your task is to find two indices i and j, both in the range [0, n - 1], that satisfy the following conditions:
+#
+# abs(i - j) >= indexDifference, and
+# abs(nums[i] - nums[j]) >= valueDifference
+# Return an integer array answer, where answer = [i, j] if there are two such indices, and answer = [-1, -1] otherwise.
+# If there are multiple choices for the two indices, return any of them.
+#
+# Note: i and j may be equal.
+def findIndices(nums, indexDifference, valueDifference):
+
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if abs(i - j) >= indexDifference and abs(nums[i] - nums[j]) >= valueDifference:
+                return [i, j]
+
+    return [-1, -1]
+
+# You are given positive integers n and m.
+#
+# Define two integers, num1 and num2, as follows:
+#
+# num1: The sum of all integers in the range [1, n] that are not divisible by m.
+# num2: The sum of all integers in the range [1, n] that are divisible by m.
+# Return the integer num1 - num2.
+def differenceOfSums(n, m):
+
+    num1 = 0
+    num2 = 0
+    for i in range(1, n + 1):
+        if i % m == 0:
+            num2 += i
+        else:
+            num1 += i
+
+    return num1 - num2
 
 
 
