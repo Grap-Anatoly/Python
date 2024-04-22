e6fd5cfd30c997f6af7350ee3514ca8431dcbc14
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-04-22 15:08:50 +0300

    LeetCode tasks, day 320

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -4997,6 +4997,23 @@ def countTestedDevices(batteryPercentages):
 
     return res
 
+# You are given a 0-indexed array mountain. Your task is to find all the peaks in the mountain array.
+#
+# Return an array that consists of indices of peaks in the given array in any order.
+#
+# Notes:
+#
+# A peak is defined as an element that is strictly greater than its neighboring elements.
+# The first and last elements of the array are not a peak.
+def findPeaks(mountain):
+
+    res = []
+    for i in range(1, len(mountain) - 1):
+        if mountain[i - 1] < mountain[i] and mountain[i] > mountain[i + 1]:
+            res.append(i)
+
+    return res
+
 
 
 
