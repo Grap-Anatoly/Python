2a56546c815a4f868968170cd81df397e59ae366
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-04-24 16:02:46 +0300

    LeetCode tasks, day 322

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -5040,6 +5040,34 @@ def numberGame(nums):
 
     return res
 
+# You are given a 0-indexed 2D integer matrix grid of size n * n with values in the range [1, n2]. Each integer
+# appears exactly once except a which appears twice and b which is missing. The task is to find the repeating and
+# missing numbers a and b.
+#
+# Return a 0-indexed integer array ans of size 2 where ans[0] equals to a and ans[1] equals to b.
+def findMissingAndRepeatedValues(grid):
+
+    oneDimm = []
+    check = []
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            oneDimm.append(grid[i][j])
+
+    check = [i for i in range(1, len(oneDimm) + 1)]
+
+    res = [0, 0]
+    for i in oneDimm:
+        if oneDimm.count(i) == 2:
+            res[0] = i
+            oneDimm.pop(oneDimm.index(i))
+            break
+
+    for i in check:
+        if i not in oneDimm:
+            res[1] = i
+            break
+
+    return res
 
 
 
