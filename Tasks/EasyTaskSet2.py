302cf36f8f28bf0aca382e25e75c7bd6aa436955
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-04-30 15:41:11 +0300

    LeetCode tasks, day 328

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -5185,6 +5185,19 @@ def incremovableSubarrayCount(nums):
 
     return res
 
+# You are given an array of integers nums of length n.
+#
+# The cost of an array is the value of its first element. For example, the cost of [1,2,3] is 1 while the cost of
+# [3,4,1] is 3.
+#
+# You need to divide nums into 3 disjoint contiguous
+# subarrays
+# .
+#
+# Return the minimum possible sum of the cost of these subarrays.
+def minimumCost(nums):
+
+    return nums[0] + sum(sorted(nums[1:])[:2])
 
 
 
