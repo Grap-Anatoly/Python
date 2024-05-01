a61bed1402fb52ca85ed3584fa7b2edc33f09eef
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-05-01 20:18:28 +0300

    LeetCode tasks, day 329

--- Tasks/EasyTaskSet2.py
+++ Tasks/EasyTaskSet2.py
@@ -5199,6 +5199,32 @@ def minimumCost(nums):
 
     return nums[0] + sum(sorted(nums[1:])[:2])
 
+# You are given a string word containing distinct lowercase English letters.
+#
+# Telephone keypads have keys mapped with distinct collections of lowercase English letters, which can be used to
+# form words by pushing them. For example, the key 2 is mapped with ["a","b","c"], we need to push the key one time
+# to type "a", two times to type "b", and three times to type "c" .
+#
+# It is allowed to remap the keys numbered 2 to 9 to distinct collections of letters. The keys can be remapped to
+# any amount of letters, but each letter must be mapped to exactly one key. You need to find the minimum number of
+# times the keys will be pushed to type the string word.
+#
+# Return the minimum number of pushes needed to type word after remapping the keys.
+#
+# An example mapping of letters to keys on a telephone keypad is given below. Note that 1, *, #, and 0 do not map
+# to any letters.
+def minimumPushes(word):
+
+    if len(word) <= 8:
+        return len(word)
+    else:
+        res = 0
+        for i in range(1, len(word) // 8 + 1):
+            res += 8 * i
+
+        res += (len(word) // 8 + 1) * (len(word) % 8)
+
+        return res
 
 
 
