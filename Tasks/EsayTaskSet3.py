aa597c34668136f5376625eb806cf1dc57f354ea
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-05-09 15:33:51 +0300

    LeetCode tasks, day 334

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -95,3 +95,23 @@ def maxOperations(nums):
                 break
 
     return len(sums)
+
+# You are given a 0-indexed integer array nums of size 3 which can form the sides of a triangle.
+#
+# A triangle is called equilateral if it has all sides of equal length.
+# A triangle is called isosceles if it has exactly two sides of equal length.
+# A triangle is called scalene if all its sides are of different lengths.
+# Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.
+def triangleType(nums):
+
+    nums = sorted(nums)
+
+    if nums[0] + nums[1] > nums[2]:
+        if len(set(nums)) == 1:
+            return "equilateral"
+        elif len(set(nums)) == 2:
+            return "isosceles"
+        else:
+            return "scalene"
+    else:
+        return "none"
