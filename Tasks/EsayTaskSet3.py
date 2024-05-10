92dd42805591b41fb5439abff1ebd927b781b7be
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-05-10 15:45:46 +0300

    LeetCode tasks, day 335

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -115,3 +115,18 @@ def triangleType(nums):
             return "scalene"
     else:
         return "none"
+
+# You are given an integer array nums of even length. You have to split the array into two parts nums1 and nums2
+# such that:
+#
+# nums1.length == nums2.length == nums.length / 2.
+# nums1 should contain distinct elements.
+# nums2 should also contain distinct elements.
+# Return true if it is possible to split the array, and false otherwise.
+def isPossibleToSplit(nums):
+
+    for i in set(nums):
+        if nums.count(i) > 2:
+            return False
+
+    return True
