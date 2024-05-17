29d742b85efc1ac64cb3f0c6f5faaa743971c1d0
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-05-17 16:42:11 +0300

    LeetCode tasks, day 341

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -235,3 +235,40 @@ def isSubstringPresent(s):
             return True
 
     return False
+
+
+# You are given a string s representing a 12-hour format time where some of the digits (possibly none)
+# are replaced with a "?".
+#
+# 12-hour times are formatted as "HH:MM", where HH is between 00 and 11, and MM is between 00 and 59.
+# The earliest 12-hour time is 00:00, and the latest is 11:59.
+#
+# You have to replace all the "?" characters in s with digits such that the time we obtain by the resulting
+# string is a valid 12-hour format time and is the latest possible.
+#
+# Return the resulting string.
+def findLatestTime(s):
+
+    s = list(s)
+
+    if s[0] == s[1] and s[0] == "?":
+        s[0] = "1"
+        s[1] = "1"
+
+    if s[0] == "?" and s[1] == "1" or s[0] == "?" and s[1] == "0":
+        s[0] = "1"
+    elif s[0] == "?":
+        s[0] = "0"
+
+    if s[1] == "?" and s[0] == '0':
+        s[1] = "9"
+    elif s[1] == "?" and s[0] == "1":
+        s[1] = "1"
+
+    if s[3] == "?":
+        s[3] = "5"
+
+    if s[4] == "?":
+        s[4] = "9"
+
+    return "".join(s)
