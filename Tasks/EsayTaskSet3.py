a2f5b0e75e233eaafce67b594159111dd31c4e17
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-05-18 16:14:07 +0300

    LeetCode tasks, day 342

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -272,3 +272,22 @@ def findLatestTime(s):
         s[4] = "9"
 
     return "".join(s)
+
+# You are given an integer array nums containing positive integers. We define a function encrypt such that encrypt(x)
+# replaces every digit in x with the largest digit in x. For example, encrypt(523) = 555 and encrypt(213) = 333.
+#
+# Return the sum of encrypted elements.
+def sumOfEncryptedInt(nums):
+
+    res = []
+
+    for i in nums:
+        i = str(i)
+        m = 0
+        for j in i:
+            m = max(m, int(j))
+
+        temp = "%s" % m * len(i)
+        res.append(int(temp))
+
+    return sum(res)
