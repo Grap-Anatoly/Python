58e82dcf60a6cb4816b66537c5c7c287049fd875
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-05-23 17:00:45 +0300

    LeetCode tasks, day 347

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -365,3 +365,46 @@ def minimumSubarrayLength(nums, k):
         return m
     else:
         return -1
+
+# A word is considered valid if:
+#
+# It contains a minimum of 3 characters.
+# It contains only digits (0-9), and English letters (uppercase and lowercase).
+# It includes at least one vowel.
+# It includes at least one consonant.
+# You are given a string word.
+#
+# Return true if word is valid, otherwise, return false.
+#
+# Notes:
+#
+# 'a', 'e', 'i', 'o', 'u', and their uppercases are vowels.
+# A consonant is an English letter that is not a vowel.
+def isValid(word):
+
+    if len(word) < 3:
+        return False
+
+    for i in "@#$":
+        if i in word:
+            return False
+
+    vowel = False
+    vowels = "aeiou"
+    for i in vowels:
+        if i in word or i.upper() in word:
+            vowel = True
+
+    if vowel == False:
+        return False
+
+    consonant = False
+    consonants = "qwrtypsdfghjklzxcvbnm"
+    for i in consonants:
+        if i in word or i.upper() in word:
+            consonant = True
+
+    if consonant == False:
+        return False
+
+    return True
