669e812b6d9684820f342e86b00e37de1f1b6b2b
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-05-24 16:39:46 +0300

    LeetCode tasks, day 348

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -408,3 +408,16 @@ def isValid(word):
         return False
 
     return True
+
+# You are given two arrays of equal length, nums1 and nums2.
+#
+# Each element in nums1 has been increased (or decreased in the case of negative) by an integer, represented by the
+# variable x.
+#
+# As a result, nums1 becomes equal to nums2. Two arrays are considered equal when they contain the same integers
+# with the same frequencies.
+#
+# Return the integer x.
+def addedInteger(nums1, nums2):
+
+    return min(nums2) - min(nums1)
