08ef2e0ff275166e904159b5785a9888db929515
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-05-26 15:42:18 +0300

    LeetCode tasks, day 350

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -444,3 +444,15 @@ def canMakeSquare(grid):
             return True
 
     return False
+
+# You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
+#
+# Return the number of special letters in word.
+def numberOfSpecialChars(word):
+
+    res = 0
+    for i in set(word.lower()):
+        if i.lower() in word and i.upper() in word:
+            res += 1
+
+    return res
