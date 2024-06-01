a18ac16f4c8e0d0c217b1788b3f5a4b8e2f96648
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-06-01 15:24:23 +0300

    LeetCode tasks, day 355

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -542,3 +542,19 @@ def isArraySpecial(nums):
             return False
 
     return True
+
+# You are given 2 integer arrays nums1 and nums2 of lengths n and m respectively. You are also given a positive
+# integer k.
+#
+# A pair (i, j) is called good if nums1[i] is divisible by nums2[j] * k (0 <= i <= n - 1, 0 <= j <= m - 1).
+#
+# Return the total number of good pairs.
+def numberOfPairs(nums1, nums2, k):
+
+    res = 0
+    for i in range(len(nums1)):
+        for j in range(len(nums2)):
+            if nums1[i] % (nums2[j] * k) == 0:
+                res += 1
+
+    return res
