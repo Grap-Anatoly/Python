60fb16952f9330bdd697472cef6ebb7419949d93
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-06-02 16:03:11 +0300

    LeetCode tasks, day 356

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -558,3 +558,25 @@ def numberOfPairs(nums1, nums2, k):
                 res += 1
 
     return res
+
+
+# You are given a string s. Simulate events at each second i:
+#
+# If s[i] == 'E', a person enters the waiting room and takes one of the chairs in it.
+# If s[i] == 'L', a person leaves the waiting room, freeing up a chair.
+# Return the minimum number of chairs needed so that a chair is available for every person who enters the waiting
+# room given that it is initially empty.
+def minimumChairs(s):
+
+    res = []
+    for i in s:
+        if i == "E":
+            if "" in res:
+                res[res.index("")] = i
+            else:
+                res.append("E")
+        else:
+            res.pop(res.index("E"))
+            res.append("")
+
+    return len(res)
