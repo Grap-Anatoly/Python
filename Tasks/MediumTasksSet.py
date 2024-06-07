17792d5a10f42267e5aeb42fb7a3f97bcd203d18
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-06-07 16:31:03 +0300

    LeetCode tasks, day 358

--- Tasks/MediumTasksSet.py
+++ Tasks/MediumTasksSet.py
@@ -321,3 +321,19 @@ def lengthOfLongestSubstring(s):
 
     return res
 
+# Given a string s, return the longest palindromic substring in s.
+def longestPalindrome(s):
+
+    if len(s) <= 1:
+        return s
+
+    ml = 1
+    ms = s[0]
+    for i in range(len(s) - 1):
+        for j in range(i + 1, len(s)):
+            if j - i + 1 > ml and s[i:j + 1] == s[i:j + 1][::-1]:
+                ml = j - i + 1
+                ms = s[i:j + 1]
+
+    return ms
+
