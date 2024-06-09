8f6a0ade01edc8b896959fa71116415761b2ebf8
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-06-09 15:22:45 +0300

    LeetCode tasks, day 360

--- Tasks/MediumTasksSet.py
+++ Tasks/MediumTasksSet.py
@@ -370,3 +370,22 @@ def convert(s, numRows):
         add += inc
 
     return "".join(res)
+
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside
+# the signed 32-bit integer range [-231, 231 - 1], then return 0.
+#
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+def reverse(x):
+
+    x = list(str(x)[::-1])
+
+    if x[-1] == "-":
+        x.insert(0, "-")
+        x.pop()
+
+    x = int("".join(x))
+
+    if -2 ** 31 < x and x < 2 ** 31 - 1:
+        return x
+    else:
+        return 0
