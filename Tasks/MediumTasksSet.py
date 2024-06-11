637a04e6df01520a7d9ab6244b506291014aa98e
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-06-11 16:29:59 +0300

    LeetCode tasks, day 362

--- Tasks/MediumTasksSet.py
+++ Tasks/MediumTasksSet.py
@@ -414,3 +414,43 @@ def threeSum(nums):
                 else:
                     k -= 1
     return res
+
+
+# Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is
+# closest to target.
+#
+# Return the sum of the three integers.
+#
+# You may assume that each input would have exactly one solution.
+def threeSumClosest(nums, target):
+    sums = set()
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            for k in range(j + 1, len(nums)):
+                sums.add(nums[i] + nums[j] + nums[k])
+
+    sums = list(sums)
+    res = min(sums, key=lambda x: abs(x - target))
+
+    return res
+
+def threeSumClosestTwoPointer(nums, target):
+
+    closest = nums[0] + nums[1] + nums[2]
+    nums = sorted(nums)
+
+    for i in range(len(nums) - 2):
+        left = i + 1
+        right = len(nums) - 1
+        while left < right:
+            s = nums[i] + nums[left] + nums[right]
+
+            if s < target:
+                left += 1
+            else:
+                right -= 1
+
+            if abs(s - target) < abs(closest - target):
+                closest = s
+
+    return closest
