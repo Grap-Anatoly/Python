b2c2a1ec4e06adfc3e9a7180f6db01ac3d2c40ae
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-07-13 18:35:26 +0300

    LeetCode tasks, day 372

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -627,3 +627,22 @@ def titleToNumber(columnTitle):
         counter -= 1
 
     return res
+
+# Given a string s and a character c that occurs in s, return an array of integers answer where answer.length ==
+# s.length and answer[i] is the distance from index i to the closest occurrence of character c in s.
+#
+# The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
+def shortestToChar(s, c):
+
+    res = []
+    indexes = []
+    for i in range(len(s)):
+        if s[i] == c:
+            indexes.append(i)
+
+    for i in range(len(s)):
+        temp = []
+        for j in indexes:
+            temp.append(abs(i - j))
+        res.append(min(temp))
+    return res
