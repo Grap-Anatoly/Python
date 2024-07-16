d69abd80dce64ff5fcf5911eb44f4841070d4ae2
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-07-16 16:56:07 +0300

    LeetCode tasks, day 375

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -688,3 +688,35 @@ def addToArrayForm(num, k):
         res.append(int(i))
 
     return res
+
+
+# You are given an array of integers stones where stones[i] is the weight of the ith stone.
+#
+# We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together.
+# Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+#
+# If x == y, both stones are destroyed, and
+# If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+# At the end of the game, there is at most one stone left.
+#
+# Return the weight of the last remaining stone. If there are no stones left, return 0.
+#
+#
+def lastStoneWeight(stones):
+
+    stones = sorted(stones)[::-1]
+
+    while len(stones) > 1:
+        if stones[0] == stones[1]:
+            stones.pop(0)
+            stones.pop(0)
+        else:
+            t = stones[0] - stones[1]
+            stones.pop(0)
+            stones[0] = t
+            stones = sorted(stones)[::-1]
+
+    if stones != []:
+        return stones[0]
+    else:
+        return 0
