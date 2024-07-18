92485c63dc217751d583330fdb0df0b1da0dcf31
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-07-18 18:52:18 +0300

    LeetCode tasks, day 376

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -720,3 +720,18 @@ def lastStoneWeight(stones):
         return stones[0]
     else:
         return 0
+
+
+# Alice and Bob take turns playing a game, with Alice starting first.
+#
+# Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+#
+# Choosing any x with 0 < x < n and n % x == 0.
+# Replacing the number n on the chalkboard with n - x.
+# Also, if a player cannot make a move, they lose the game.
+#
+# Return true if and only if Alice wins the game, assuming both players play optimally.
+def divisorGame(n):
+
+    return n % 2 == 0
+
