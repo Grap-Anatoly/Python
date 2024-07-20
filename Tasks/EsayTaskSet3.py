0b4466e9593b00c5f3ad6ce4a605c64e873b6884
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-07-20 16:09:33 +0300

    LeetCode tasks, day 377

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -735,3 +735,29 @@ def divisorGame(n):
 
     return n % 2 == 0
 
+
+# Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+#
+# Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do
+# not appear in arr2 should be placed at the end of arr1 in ascending order.
+def relativeSortArray(arr1, arr2):
+
+    arr1Unique = []
+    arr1Count = {}
+    for i in arr1:
+        if i not in arr2:
+            arr1Unique.append(i)
+
+    for i in arr2:
+        arr1Count[i] = arr1.count(i)
+
+    arr1Unique = sorted(arr1Unique)
+
+    res = []
+    for i in arr2:
+        for j in range(arr1Count[i]):
+            res.append(i)
+
+    return res + arr1Unique
+
+
