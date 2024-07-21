b5a80dd08a9221041ecf2cfecba597982b5010c3
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-07-21 17:32:46 +0300

    LeetCode tasks, day 378

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -761,3 +761,22 @@ def relativeSortArray(arr1, arr2):
     return res + arr1Unique
 
 
+# You are given an array of strings words and a string chars.
+#
+# A string is good if it can be formed by characters from chars (each character can only be used once).
+#
+# Return the sum of lengths of all good strings in words.
+def countCharacters(words, chars):
+
+    res = 0
+    for i in words:
+        check = True
+        for j in i:
+            if i.count(j) > chars.count(j):
+                check = False
+                break
+        if check == True:
+            res += len(i)
+
+    return res
+
