17e78e5be6efbe0e0d6fe1c645484be46754b6ac
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-07-23 16:23:23 +0300

    LeetCode tasks, day 380

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -800,3 +800,25 @@ def dayOfYear(date):
     res += int(date[8:10])
 
     return res
+
+
+# We have n chips, where the position of the ith chip is position[i].
+#
+# We need to move all the chips to the same position. In one step, we can change the position of the ith chip from
+# position[i] to:
+#
+# position[i] + 2 or position[i] - 2 with cost = 0.
+# position[i] + 1 or position[i] - 1 with cost = 1.
+# Return the minimum cost needed to move all the chips to the same position.
+def minCostToMoveChips(position):
+
+    even = 0
+    odd = 0
+
+    for i in position:
+        if i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+
+    return min(even, odd)
