dab011fd1dac50e0772ed6d46e40d6631d3e9a9b
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-07-26 20:21:31 +0300

    LeetCode tasks, day 382

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -851,3 +851,23 @@ def numPrimeArrangements(n):
             np.append(i)
 
     return math.factorial(len(p)) * math.factorial(len(np)) % (10 ** 9 + 7)
+
+# You are given a 0-indexed string array words, where words[i] consists of lowercase English letters.
+#
+# In one operation, select any index i such that 0 < i < words.length and words[i - 1] and words[i] are anagrams,
+# and delete words[i] from words. Keep performing this operation as long as you can select an index that satisfies
+# the conditions.
+#
+# Return words after performing all operations. It can be shown that selecting the indices for each operation in any
+# arbitrary order will lead to the same result.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase using all
+# the original letters exactly once. For example, "dacb" is an anagram of "abdc".
+def removeAnagrams(words):
+
+    res = []
+    for i in range(0, len(words)):
+        if i == 0 or sorted(words[i]) != sorted(words[i - 1]):
+            res.append(words[i])
+
+    return res
