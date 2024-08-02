fb508def14e56b1e0b805af04799b26c5b0512fc
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-08-02 19:23:20 +0300

    LeetCode tasks, day 387

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -964,3 +964,23 @@ def removePalindromeSub(s):
         return 1
     else:
         return 2
+
+
+# Given an integer n and an integer array rounds. We have a circular track which consists of n sectors labeled from
+# 1 to n. A marathon will be held on this track, the marathon consists of m rounds. The ith round starts at sector
+# rounds[i - 1] and ends at sector rounds[i]. For example, round 1 starts at sector rounds[0]
+# and ends at sector rounds[1]
+#
+# Return an array of the most visited sectors sorted in ascending order.
+#
+# Notice that you circulate the track in ascending order of sector numbers in the counter-clockwise direction
+# (See the first example).
+def mostVisited(n, rounds):
+
+    first = rounds[0]
+    last = rounds[-1]
+
+    if first <= last:
+        return range(first, last + 1)
+    else:
+        return list(range(1, last + 1)) + list(range(first, n + 1))
