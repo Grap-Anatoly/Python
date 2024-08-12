91664ea996f1d2ee74769a91d82dcb50024670bd
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-08-12 15:30:00 +0300

    LeetCode tasks, day 388

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -984,3 +984,25 @@ def mostVisited(n, rounds):
         return range(first, last + 1)
     else:
         return list(range(1, last + 1)) + list(range(first, n + 1))
+
+# On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds
+# to visit all the points in the order given by points.
+#
+# You can move according to these rules:
+#
+# In 1 second, you can either:
+# move vertically by one unit,
+# move horizontally by one unit, or
+# move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
+# You have to visit the points in the same order as they appear in the array.
+# You are allowed to pass through points that appear later in the order, but these do not count as visits.
+def minTimeToVisitAllPoints(points):
+
+    res = 0
+    for i in range(len(points) - 1):
+        xd = abs(points[i + 1][0] - points[i][0])
+        yd = abs(points[i + 1][1] - points[i][1])
+
+        res += max(xd, yd)
+
+    return res
