f72b276cfe3226ae21d945fe263b3baae1b75f53
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-08-13 15:54:10 +0300

    LeetCode tasks, day 389

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1006,3 +1006,43 @@ def minTimeToVisitAllPoints(points):
         res += max(xd, yd)
 
     return res
+
+# You are given a string s. Reorder the string using the following algorithm:
+#
+# Pick the smallest character from s and append it to the result.
+# Pick the smallest character from s which is greater than the last appended character to the result and append it.
+# Repeat step 2 until you cannot pick more characters.
+# Pick the largest character from s and append it to the result.
+# Pick the largest character from s which is smaller than the last appended character to the result and append it.
+# Repeat step 5 until you cannot pick more characters.
+# Repeat the steps from 1 to 6 until you pick all characters from s.
+# In each step, If the smallest or the largest character appears more than once you can choose any occurrence and
+# append it to the result.
+#
+# Return the result string after sorting s with this algorithm.
+def sortString(s):
+
+    charsFreq = {}
+    for i in s:
+        if i in charsFreq:
+            charsFreq[i] += 1
+        else:
+            charsFreq[i] = 1
+
+    res = ""
+    asc = True
+    while any(charsFreq.values()) == True:
+        if asc == True:
+            for i in "abcdefghijklmnopqrstuvwxyz":
+                if i in charsFreq and charsFreq[i] > 0:
+                    res += i
+                    charsFreq[i] -= 1
+            asc = False
+        else:
+            for i in "abcdefghijklmnopqrstuvwxyz"[::-1]:
+                if i in charsFreq and charsFreq[i] > 0:
+                    res += i
+                    charsFreq[i] -= 1
+            asc = True
+
+    return res
