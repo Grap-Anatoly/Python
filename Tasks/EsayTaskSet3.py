3e710bc7e034c09e06a4d4f5611e0de9a2e59271
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-08-14 15:32:27 +0300

    LeetCode tasks, day 390

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1046,3 +1046,35 @@ def sortString(s):
             asc = True
 
     return res
+
+# Given a string path, where path[i] = 'N', 'S', 'E' or 'W', each representing moving one unit north, south, east,
+# or west, respectively. You start at the origin (0, 0) on a 2D plane and walk on the path specified by path.
+#
+# Return true if the path crosses itself at any point, that is, if at any time you are on a location you have
+# previously visited. Return false otherwise.
+def isPathCrossing(path):
+
+    coord = [[0, 0]]
+
+    for i in path:
+        if i == "N":
+            last = list(coord[-1])
+            last[0] += 1
+            coord.append(last)
+        if i == "E":
+            last = list(coord[-1])
+            last[1] += 1
+            coord.append(last)
+        if i == "S":
+            last = list(coord[-1])
+            last[0] -= 1
+            coord.append(last)
+        if i == "W":
+            last = list(coord[-1])
+            last[1] -= 1
+            coord.append(last)
+
+        if coord.count(coord[-1]) > 1:
+            return True
+
+    return False
