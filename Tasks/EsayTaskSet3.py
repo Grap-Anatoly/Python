226eacb20004e4a69295d991f7fced8352e3947d
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-08-20 21:22:43 +0300

    LeetCode tasks, day 395

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1162,3 +1162,20 @@ def frequencySort(nums):
     nums.sort(key=lambda x: dct[x])
 
     return nums
+
+# Given n points on a 2D plane where points[i] = [xi, yi], Return the widest vertical area between two points
+# such that no points are inside the area.
+#
+# A vertical area is an area of fixed-width extending infinitely along the y-axis (i.e., infinite height).
+# The widest vertical area is the one with the maximum width.
+#
+# Note that points on the edge of a vertical area are not considered included in the area.
+def maxWidthOfVerticalArea(points):
+
+    points = sorted(points)
+
+    res = 0
+    for i in range(len(points) - 1):
+        res = max(res, points[i + 1][0] - points[i][0])
+
+    return res
