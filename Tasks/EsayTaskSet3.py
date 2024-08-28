fcaacf87283ceab1b5898ef0c369e975635a787f
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-08-28 20:34:46 +0300

    LeetCode tasks, day 403

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1339,3 +1339,25 @@ def minimumDifference(nums, k):
             res = min(res, nums[i] - nums[i - k + 1])
 
         return res
+
+# There are n availabe seats and n students standing in a room. You are given an array seats of length n,
+# where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+#
+# You may perform the following move any number of times:
+#
+# Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position
+# x to x + 1 or x - 1)
+# Return the minimum number of moves required to move each student to a seat such that no two students
+# are in the same seat.
+#
+# Note that there may be multiple seats or students in the same position at the beginning.
+def minMovesToSeat(seats, students):
+
+    seats = sorted(seats)
+    students = sorted(students)
+
+    res = 0
+    for i in range(len(seats)):
+        res += abs(students[i] - seats[i])
+
+    return res
