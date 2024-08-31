7a006d37907db8627a33b2863808a67be873ca61
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-08-31 23:12:42 +0300

    LeetCode tasks, day 405

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1390,3 +1390,22 @@ def countVowelSubstrings(word):
             res += 1
 
     return res
+
+# Given an integer array hours representing times in hours, return an integer denoting the number of pairs i,
+# j where i < j and hours[i] + hours[j] forms a complete day.
+#
+# A complete day is defined as a time duration that is an exact multiple of 24 hours.
+#
+# For example, 1 day is 24 hours, 2 days is 48 hours, 3 days is 72 hours, and so on.
+#
+#
+def countCompleteDayPairs(hours):
+
+    res = []
+    for i in range(len(hours)):
+        for j in range(i, len(hours)):
+            if i < j:
+                if (hours[i] + hours[j]) % 24 == 0:
+                    res.append([hours[i], hours[j]])
+
+    return len(res)
