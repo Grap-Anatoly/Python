eb0af78553c20950cc212f49f1b127682850929e
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-09-12 15:58:23 +0300

    LeetCode tasks, day 413

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1615,7 +1615,23 @@ def imageSmoother(img):
         res.append(t)
     return res
 
+# A school is trying to take an annual photo of all the students. The students are asked to stand in a single
+# file line in non-decreasing order by height. Let this ordering be represented by the integer array expected
+# where expected[i] is the expected height of the ith student in line.
+#
+# You are given an integer array heights representing the current order that the students are standing in.
+# Each heights[i] is the height of the ith student in line (0-indexed).
+#
+# Return the number of indices where heights[i] != expected[i].
+def heightChecker(heights):
 
+    sh = sorted(heights)
+    res = 0
+    for i in range(len(heights)):
+        if sh[i] != heights[i]:
+            res += 1
+
+    return res
 
 
 
