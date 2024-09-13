c9f4c37379e32e40c9eedd9e55854fdf26f60e4c
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-09-13 15:16:26 +0300

    LeetCode tasks, day 414

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1633,6 +1633,31 @@ def heightChecker(heights):
 
     return res
 
+# You are given an n x n grid where you have placed some 1 x 1 x 1 cubes. Each value v = grid[i][j] represents a
+# tower of v cubes placed on top of cell (i, j).
+#
+# After placing these cubes, you have decided to glue any directly adjacent cubes to each other, forming several
+# irregular 3D shapes.
+#
+# Return the total surface area of the resulting shapes.
+#
+# Note: The bottom face of each shape counts toward its surface area.
+def surfaceArea(grid):
+
+    n = len(grid)
+    res = 0
+
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] > 0:
+                res += 2 + grid[i][j] * 4
+            if i > 0:
+                res -= min(grid[i - 1][j], grid[i][j]) * 2
+            if j > 0:
+                res -= min(grid[i][j - 1], grid[i][j]) * 2
+
+    return res
+
 
 
 
