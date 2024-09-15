9656c37ba9bf1a5fdc9a1ae30f686357b6c6e6d6
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-09-15 15:22:08 +0300

    LeetCode tasks, day 416

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1686,6 +1686,20 @@ def gcdOfStrings(str1, str2):
 
     return res
 
+# Given two strings first and second, consider occurrences in some text of the form "first second third",
+# where second comes immediately after first, and third comes immediately after second.
+#
+# Return an array of all the words third for each occurrence of "first second third".
+def findOcurrences(text, first, second):
+
+    text = text.split(" ")
+    res = []
+    for i in range(len(text) - 2):
+        if text[i] == first and text[i + 1] == second:
+            res.append(text[i + 2])
+
+    return res
+
 
 
 
