6086c4e8c2f79002c7f47f9a4058ed18153e9509
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-09-19 15:33:42 +0300

    LeetCode tasks, day 418

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1722,3 +1722,61 @@ def duplicateZeros(arr):
         else:
             counter += 1
 
+
+# We distribute some number of candies, to a row of n = num_people people in the following way:
+#
+# We then give 1 candy to the first person, 2 candies to the second person, and so on until we give n candies to
+# the last person.
+#
+# Then, we go back to the start of the row, giving n + 1 candies to the first person, n + 2 candies to the second
+# person, and so on until we give 2 * n candies to the last person.
+#
+# This process repeats (with us giving one more candy each time, and moving to the start of the row after we reach
+# the end) until we run out of candies.  The last person will receive all of our remaining candies (not necessarily
+# one more than the previous gift).
+#
+# Return an array (of length num_people and sum candies) that represents the final distribution of candies.
+def distributeCandies(candies, num_people):
+
+    res = [0] * num_people
+    counter = 0
+    candy = 1
+
+    while candies > 0:
+        if counter < len(res):
+
+            res[counter] += candy
+            candy += 1
+
+            left = candies - candy
+            if left <= 0:
+                if counter + 1 < len(res):
+                    res[counter + 1] += candies - 1
+                    break
+                else:
+                    res[0] += candies - 1
+                    break
+            else:
+                counter += 1
+                candies = left
+        else:
+
+            counter = 0
+            res[counter] += candy
+            candy += 1
+
+            left = candies - candy
+            if left < 0:
+                if counter + 1 <= len(res):
+                    res[counter + 1] += candies - 1
+                    break
+                else:
+                    res[0] += candies - 1
+                    break
+            else:
+                counter += 1
+                candies = left
+
+    return res
+
+
