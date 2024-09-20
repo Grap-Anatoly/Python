501e09913c3b8a75ecd8067d3454917cc9363fd5
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-09-20 14:57:32 +0300

    LeetCode tasks, day 419

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1780,3 +1780,42 @@ def distributeCandies(candies, num_people):
     return res
 
 
+# Write a program to count the number of days between two dates.
+#
+# The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
+def daysBetweenDates(date1, date2):
+
+    res = 0
+
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if date1 > date2:
+        date1, date2 = date2, date1
+
+    y1, m1, d1 = date1.split("-")
+    y2, m2, d2 = date2.split("-")
+
+    y1, m1, d1, y2, m2, d2 = int(y1), int(m1), int(d1), int(y2), int(m2), int(d2)
+
+    for i in range(y1, y2):
+        res += 365
+        if i % 4 == 0 and i % 100 != 0 or i % 400 == 0:
+            res += 1
+
+    if y1 % 4 == 0 and y1 % 100 != 0 or y1 % 400 == 0:
+        days[1] = 29
+    else:
+        days[1] = 28
+
+    res -= sum(days[:m1 - 1]) + d1
+
+    if y2 % 4 == 0 and y2 % 100 != 0 or y2 % 400 == 0:
+        days[1] = 29
+    else:
+        days[1] = 28
+
+    res += sum(days[:m2 - 1]) + d2
+
+    return res
+
+
