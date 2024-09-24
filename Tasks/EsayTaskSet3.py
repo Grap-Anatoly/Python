a7ebafe2c5f03edcf94e41b75e215671ebfcf5e0
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-09-24 19:56:48 +0300

    LeetCode tasks, day 421

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1879,4 +1879,30 @@ def convertTime(current, correct):
 
         return res
 
+# There is a circle of red and blue tiles. You are given an array of integers colors. The color of tile i is
+# represented by colors[i]:
+#
+# colors[i] == 0 means that tile i is red.
+# colors[i] == 1 means that tile i is blue.
+# Every 3 contiguous tiles in the circle with alternating colors (the middle tile has a different color from
+# its left and right tiles) is called an alternating group.
+#
+# Return the number of alternating groups.
+#
+# Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+def numberOfAlternatingGroups(colors):
+    res = 0
+    for i in range(len(colors)):
+        if i == 0:
+            if colors[-1] != colors[i] and colors[i] != colors[i + 1]:
+                res += 1
+        elif i == len(colors) - 1:
+            if colors[0] != colors[i] and colors[i] != colors[i - 1]:
+                res += 1
+        else:
+            if colors[i - 1] != colors[i] and colors[i] != colors[i + 1]:
+                res += 1
+
+    return res
+
 
