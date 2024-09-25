4e01a53fffde4d20e118509cda7afcaea8a2dbe8
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-09-25 23:06:30 +0300

    LeetCode tasks, day 422

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1905,4 +1905,19 @@ def numberOfAlternatingGroups(colors):
 
     return res
 
+# You are given a string s and an integer k. Encrypt the string using the following algorithm:
+#
+# For each character c in s, replace c with the kth character after c in the string (in a cyclic manner).
+# Return the encrypted string
+def getEncryptedString(self, s: str, k: int) -> str:
+
+        if len(set(s)) == 1:
+            return s
+        else:
+            res = []
+            for i in range(len(s)): 
+                res.append(s[(i+k) % len(s)])
+            
+            return "".join(res)
+
 
