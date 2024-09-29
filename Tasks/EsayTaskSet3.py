9f8cfc3770f9816229d93b2cd360608de7f22c9d
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-09-29 15:15:14 +0300

    LeetCode tasks, day 424

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -1980,5 +1980,31 @@ def maxHeightOfTriangle(red, blue):
 
         return max(resR, resB)
 
+# Given a string s containing only digits, return the
+# lexicographically smallest string
+#  that can be obtained after swapping adjacent digits in s with the same parity at most once.
+#
+# Digits have the same parity if both are odd or both are even. For example, 5 and 9, as well as 2 and 4,
+# have the same parity, while 6 and 9 do not.
+def getSmallestString(s):
+    s = list(s)
+
+    for i in range(len(s) - 1):
+        if int(s[i]) % 2 == 0 and int(s[i + 1]) % 2 == 0:
+            temp = list(s)
+            t = temp[i]
+            temp[i] = temp[i + 1]
+            temp[i + 1] = t
+            if temp < s:
+                return "".join(temp)
+        if int(s[i]) % 2 != 0 and int(s[i + 1]) % 2 != 0:
+            temp = list(s)
+            t = temp[i]
+            temp[i] = temp[i + 1]
+            temp[i + 1] = t
+            if temp < s:
+                return "".join(temp)
+
+    return "".join(s)
 
 
