07b56ade1656d920aafce0eb38bd6aa55201babc
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-10-14 18:12:43 +0300

    LeetCode tasks, day 431

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -2138,3 +2138,19 @@ def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
         return True
     else:
         return False      	
+
+# You are given an integer array nums, an integer k, and an integer multiplier.
+# 
+# You need to perform k operations on nums. In each operation:
+# 
+# Find the minimum value x in nums. If there are multiple occurrences of the minimum value, select the one that appears first.
+# Replace the selected minimum value x with x * multiplier.
+# Return an integer array denoting the final state of nums after performing all k operations.
+def getFinalState(nums, k, multiplier):
+
+    for i in range(k):
+        m = min(nums)
+        pos = nums.index(m)
+        nums[pos] = m * multiplier
+        
+    return nums
