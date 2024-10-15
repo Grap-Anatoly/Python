bc95401feecfca95d47f21abfc1aea14cc31a0cf
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-10-15 17:39:49 +0300

    LeetCode tasks, day 432

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -2154,3 +2154,19 @@ def getFinalState(nums, k, multiplier):
         nums[pos] = m * multiplier
         
     return nums
+
+# You are given a string date representing a Gregorian calendar date in the yyyy-mm-dd format.
+#
+# date can be written in its binary representation obtained by converting year, month, and day to their binary 
+# representations without any leading zeroes and writing them down in year-month-day format.
+# 
+# Return the binary representation of date.
+def convertDateToBinary(date):
+
+    date = date.split("-")
+    conv = []
+    for i in date:
+        conv.append(bin(int(i))[2:])
+        conv.append("-")
+
+    return "".join(conv)[:-1]
