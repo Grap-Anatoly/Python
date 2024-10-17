762693a6c5c7aedb599554e61632a86becac1be1
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-10-17 18:33:21 +0300

    LeetCode tasks, day 434

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -2193,3 +2193,16 @@ def generateKey(num1, num2, num3):
         res.append(min(nums[0][i], nums[1][i], nums[2][i]))
 
     return int("".join(res))
+
+# In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. 
+# Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an additional time, making the list longer than usual.
+# 
+# As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
+def getSneakyNumbers(nums):
+
+    res = []
+    for i in set(nums):
+        if nums.count(i) == 2:
+            res.append(i)
+        if len(res) == 2:
+            return res
