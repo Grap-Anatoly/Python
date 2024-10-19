e43df581590512c2afb8d4043224f9a117b9c889
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-10-19 16:07:07 +0300

    LeetCode tasks, day 435

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -2206,3 +2206,19 @@ def getSneakyNumbers(nums):
             res.append(i)
         if len(res) == 2:
             return res
+
+# There are n mountains in a row, and each mountain has a height. You are given an integer array height where height[i] 
+# represents the height of mountain i, and an integer threshold.
+# 
+# A mountain is called stable if the mountain just before it (if it exists) has a height strictly greater than threshold. 
+# Note that mountain 0 is not stable.
+# 
+# Return an array containing the indices of all stable mountains in any order.
+def stableMountains(height, threshold):
+
+    res = []
+    for i in range(1, len(height)):
+        if height[i-1] > threshold:
+            res.append(i)
+        
+    return res
