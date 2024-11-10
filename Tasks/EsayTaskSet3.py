ac50478fd7d76c2eb5a99b6a6336e09632b76b6d
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-11-10 17:30:37 +0300

    LeetCode tasks, day 439

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -2286,3 +2286,16 @@ def minElement(nums):
         res = min(res,t)
     
     return res
+
+# You are given two integers n and t. Return the smallest number greater than or equal to n such that the product of its digits is divisible by t.
+def smallestNumber(n, t):
+    while self.product(n) % t != 0:
+        n += 1
+    return n
+
+def product(self, n):
+    prod = 1
+    while n > 0:
+        prod *= n % 10
+        n //= 10
+    return prod
