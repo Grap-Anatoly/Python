70a1274f1f28a92b4296b9cdf0cd06d60deb91a2
Anatoly Bilonig <39996329+Grap-Anatoly@users.noreply.github.com>
2024-12-02 20:06:36 +0300

    LeetCode tasks, day 444

--- Tasks/EsayTaskSet3.py
+++ Tasks/EsayTaskSet3.py
@@ -2349,3 +2349,28 @@ def smallestNumber(n):
         counter += 1
 
     return counter
+
+# Alice and Bob are playing a game where they take turns removing stones from a pile, with Alice going first.
+#
+# Alice starts by removing exactly 10 stones on her first turn.
+# For each subsequent turn, each player removes exactly 1 fewer stone than the previous opponent.
+# The player who cannot make a move loses the game.
+# 
+# Given a positive integer n, return true if Alice wins the game and false otherwise.
+def canAliceWin(n):
+
+    turn = 0
+    stones = 10
+
+    while n >= stones:
+        turn += 1
+        n -= stones
+        stones -= 1
+
+    if turn == 0:
+        return False
+    else:
+        if turn % 2 == 0:
+            return False
+        else:
+            return True
